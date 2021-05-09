7d49330b0d34c9e140ed2771a1250676971c459d
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-09 23:15:10 +0300

    Create Term-sett.sh

---
+++ Term-sett.sh
@@ -0,0 +1,81 @@
+#!/data/data/com.termux/files/usr/bin/bash
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+${ORANGE}
+clear
+sleep 3
+echo "Installing..."
+sleep 1
+echo "please wait..."
+sleep 2
+cd
+apt-get update
+apt-get upgrade
+apt-get upgraded
+apt-get install git
+apt-get install python2
+apt-get install python
+apt-get install php
+apt-get install openssh
+apt-get install curl
+apt-get install ssl
+apt-get install python3
+apt-get install colorama
+apt-get install toilet
+apt-get install w3m
+apt-get install pip
+apt-get install apache2 
+sleep 1
+echo Loading please wait. Installing tool.
+${BLACKBG}
+sleep 1
+apt install jq 
+apt install curl 
+apt install zip 
+pkg install wget 
+gem install lolcat
+apt install figlet 
+apt install ruby
+sleep 1
+echo SuccesFull. I use termux!
+sleep 1
+echo Installing DarkFly-Tool...
+sleep 2
+https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+cd
+sleep 1
+echo Installing shark...
+sleep 1
+git clone https://github.com/Bhaviktutorials/shark
+cd shark
+chmod +x *
+./setup
+cd
+sleep 1
+echo Installing DarkDump...
+sleep 1
+git clone https://github.com/josh0xA/darkdump
+cd darkdump
+pip install -r requirements.txt
+cd
+cd
+clear
+sleep 2
+echo Compiling command...
+sleep 1
+echo setting termux please wait...
+sleep 2
+toilet -F gay Done
+${RED}
+echo author by mishakorzhik
+echo gmail misakorzhik528@gmail.com
+echo github https://github.com/mishakorzik
+sleep 4
+echo Run command: "DarkFly"
+echo Run command: "cd shark && shark"
+echo Run command: "cd darkdump && python darkdump.py"
