ad4484b92ab202ff444f8b0ebade26599e47861e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-10 11:09:36 +0300

    Create AutoInstallTool.sh

---
+++ AutoInstallTool.sh
@@ -0,0 +1,44 @@
+clear
+echo By mishakorzhik
+sleep 2
+echo Loading please wait. Settings tool.
+sleep 1
+cd
+apt update
+apt upgrade
+apt upgraded
+sleep 1
+echo Installing tools.
+sleep 1
+pkg install python2
+pkg install git
+pkg install python3
+pkg install python
+pkg install pip
+pkg install curl
+sleep 1
+echo Installing Impulse.
+sleep 1
+git clone https://github.com/LimerBoy/Impulse
+cd Impulse
+pip3 install -r requirements.txt
+cd
+sleep 1
+echo Installing SocialBox-Termux.
+sleep 1
+git clone https://github.com/samsesh/SocialBox-Termux.git 
+cd SocialBox-Termux
+chmod +x install-sb.sh
+./install-sb.sh
+cd
+sleep 1
+echo Installing haxorbd.
+sleep 1
+git clone https://github.com/htr-tech/haxorbd.git
+cd
+sleep 1 
+echo Installing
+sleep 1
+echo Run command: cd haxorbd && python2 haxor.py
+echo Run command: cd SocialBox-Termux && ./SocialBox.sh
+echo Run command: python3 impulse.py --method SMS --time 20 --threads 15 --target Номер
