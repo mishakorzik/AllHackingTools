691ca1db09a80b02a26f7daeec50caea1c01e718
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-10 10:08:42 +0300

    Update Term-sett.sh

--- Term-sett.sh
+++ Term-sett.sh
@@ -1,14 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
-RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
-MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
-REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
-MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
-RESETBG="$(printf '\e[0m\n')"
 clear
 sleep 3
 echo "Installing..."
-${ORANGE}
-${ORANGE}
+ORANGE="$(printf '\033[33m')" ${ORANGE}
+ORANGE="$(printf '\033[33m')" ${ORANGE}
 sleep 1
 echo "please wait..."
 sleep 2
@@ -31,8 +26,8 @@ apt-get install pip
 apt-get install apache2 
 sleep 1
 echo Loading please wait. Installing tool.
-${BLACKBG}
-${BLACKBG}
+BLACKBG="$(printf '\033[40m')" ${BLACKBG}
+BLACKBG="$(printf '\033[40m')" ${BLACKBG}
 sleep 1
 apt install jq 
 apt install curl 
@@ -76,8 +71,8 @@ sleep 1
 echo setting termux please wait...
 sleep 2
 toilet -F gay Done
-${RED}
-${RED}
+RED="$(printf '\033[31m')" ${RED}
+RED="$(printf '\033[31m')" ${RED}
 echo author by mishakorzhik
 echo gmail misakorzhik528@gmail.com
 echo github https://github.com/mishakorzik
