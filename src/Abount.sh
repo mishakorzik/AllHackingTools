7f995516b9c924fe2e8ec9cabf237365f6bc07bf
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-18 22:52:46 +0300

    Create Abount.sh

---
+++ src/Abount.sh
@@ -0,0 +1,17 @@
+
+echo -n ${BLUE}"
+    ┌──────────────────────────────────────────────────────────────┐
+    │  			     ┏━┓┏┓ ┏━┓╻ ╻╺┳╸			   │
+    │  			     ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 			   │
+    │  			     ╹ ╹┗━┛┗━┛┗━┛ ╹ 			   │
+    │──────────────────────────────────────────────────────────────│
+    │                                                              │
+    │ >>  'Hi, here you can see my telegram and github there       |
+    |       will be questions you will write in telegram.          │
+    │                                                              │
+    │ >>  Author   :  Misha Korzhik                                │
+    │     Github   :  https://github.com/mishakorzik               │
+    │     Telegram :  pseudonym @MishaKorzhikTelegram              │
+    │                                                              │
+    └──────────────────────────────────────────────────────────────┘"
+"    
