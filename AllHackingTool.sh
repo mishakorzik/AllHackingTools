88554d371e1f47e11c0f04e758bc9ba503014a13
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-23 11:08:24 +0300

    Create AllHackingTool.sh

---
+++ AllHackingTool.sh
@@ -0,0 +1,32 @@
+clear
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+echo "╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╱╱╱╱╱╭━━━━╮╱╱╭━━━╮╭╮╱╱╱╱╱╭╮"
+echo "┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱┃╭━━╯╱╭╯╰╮╱╱╱╱╱┃╭╮╭╮┃╱╱┃╭━╮┣╯╰╮╱╱╱╭╯╰╮"
+echo "┃╰━╯┣━┳━━┳━━┳━━╮┃╰━━┳━╋╮╭╋━━┳━╮╰╯┃┃┣┻━╮┃╰━━╋╮╭╋━━┳┻╮╭╯"
+echo "┃╭━━┫╭┫┃━┫━━┫━━┫┃╭━━┫╭╮┫┃┃┃━┫╭╯╱╱┃┃┃╭╮┃╰━━╮┃┃┃┃╭╮┃╭┫┃"
+echo "┃┃╱╱┃┃┃┃━╋━━┣━━┃┃╰━━┫┃┃┃╰┫┃━┫┃╱╱╱┃┃┃╰╯┃┃╰━╯┃┃╰┫╭╮┃┃┃╰╮"
+echo "╰╯╱╱╰╯╰━━┻━━┻━━╯╰━━━┻╯╰┻━┻━━┻╯╱╱╱╰╯╰━━╯╰━━━╯╰━┻╯╰┻╯╰━╯"
+echo "Please press enter to launch..."
+read a1
+echo -e $b">"$w" verify modules: "$g"python2"$w
+apt-get install python
+echo -e $b">"$w" verify modules: "$g"pip3"$w
+apt-get install pip3
+echo -e $b">"$w" verify modules: "$g"requests"$w
+pip3 install requests
+echo -e $b">"$w" verify modules: "$g"php"$w
+apt-get install php
+echo -e $b">"$w" verify modules: "$g"bs4"$w
+apt-get install bs4
+echo -e $b">"$w" verify modules: "$g"wget"$w
+apt-get install wget
+echo -e $b">"$w" verify modules: "$g"openssh"$w
+apt-get install openssh
+echo "Succesfull verifined!"
+clear
+python2 Logo.py
+sleep 1
+python2 MainMenu.py
