55844f2867dbb5fa545a322547a95b72e762a77e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-23 11:14:19 +0300

    Create Modules.sh

---
+++ Modules.sh
@@ -0,0 +1,46 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+apt-get install git 
+echo -e $b">"$w" installing modules: "$g"python"$w
+apt-get install python
+echo -e $b">"$w" installing modules: "$g"wget"$w
+apt-get install wget 
+echo -e $b">"$w" installing modules: "$g"openssh"$w
+apt-get install openssh 
+echo -e $b">"$w" installing modules: "$g"php"$w
+apt-get install php
+echo -e $b">"$w" installing modules: "$g"toilet"$w
+apt-get install toilet
+echo -e $b">"$w" installing modules: "$g"colorama"$w
+apt-get install colorama
+echo -e $b">"$w" installing modules: "$g"jq"$w
+apt-get install jq 
+echo -e $b">"$w" installing modules: "$g"apache2"$w
+apt-get install apache2
+echo -e $b">"$w" installing modules: "$g"ruby"$w
+apt-get install ruby 
+echo -e $b">"$w" installing modules: "$g"ssl"$w
+apt-get install ssl
+echo -e $b">"$w" installing modules: "$g"w3m"$w
+apt-get install w3m 
+echo -e $b">"$w" installing modules: "$g"figlet"$w
+apt-get install figlet
+echo -e $b">"$w" installing modules: "$g"zip"$w
+apt-get install zip 
+echo -e $b">"$w" installing modules: "$g"lolcat"$w
+apt-get install lolcat
+echo -e $b">"$w" installing modules: "$g"pv"$w
+apt-get install pv 
+echo -e $b">"$w" installing modules: "$g"neofetch"$w
+apt-get install neofetch
+echo -e $b">"$w" installing modules: "$g"zsh"$w
+apt-get install zsh
+
+
+
+
+
+
