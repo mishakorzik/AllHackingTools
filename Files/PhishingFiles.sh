a2f49b7eebede0c0712f27baa63d769dca12119b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-23 22:36:08 +0300

    Create PhishingFiles.sh

---
+++ Files/PhishingFiles.sh
@@ -0,0 +1,17 @@
+apt-get install git
+cd
+cd AllHackingTools
+git clone https://github.com/Bhaviktutorials/shark
+cd shark chmod +x *
+./setup
+cd
+cd AllHackingTools
+git clone git://github.com/htr-tech/zphisher.git
+cd
+cd AllHackingTools
+git clone https://github.com/AbirHasan2005/ShellPhish
+cd ShellPhish 
+chmod +x * 
+Python2 PhishingMenu.py
+
+
