7af3a68d06132ecb38ed91faf913e2ffd950513d
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-23 10:52:47 +0300

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -19,22 +19,49 @@ print("  # \033[1;34m[ 4 ] >> \033[1;36;40mWeb-DDOS")
 print("  # \033[1;34m[ 5 ] >> \033[1;36;40mWeb-AdminHack")
 print("  # \033[1;34m[ 6 ] >> \033[1;36;40mAndroid-Hack")
 print("  # \033[1;34m[ 7 ] >> \033[1;36;40mSQL injection-Tool")
-print("  # \033[1;34m[ 8 ] >> \033[1;36;40mSocialMefia-Finder")
-print("  # \033[1;34m[ 9 ] >> \033[1;36;40mDarkSearch-Tool")
-print("  # \033[1;34m[ 10 ] >> \033[1;36;40mMail-Hack")
-print("  # \033[1;34m[ 11 ] >> \033[1;36;40m")
-print("  # \033[1;34m[ 12 ] >> \033[1;36;40mEXIT")
-print("  # \033[1;34m[ 13 ] >> \033[1;36;40mEXIT")
-print("  # \033[1;34m[ 14 ] >> \033[1;36;40mEXIT")
-print("  # \033[1;34m[ 15 ] >> \033[1;36;40mEXIT")
+print("  # \033[1;34m[ 8 ] >> \033[1;36;40mSocialMedia-Finder")
+print("  # \033[1;34m[ 9 ] >> \033[1;36;40mSocialMedia-Attack")
+print("  # \033[1;34m[ 10 ] >> \033[1;36;40mDarkSearch-Tool")
+print("  # \033[1;34m[ 11 ] >> \033[1;36;40mMail-Hack")
+print("  # \033[1;34m[ 12 ] >> \033[1;36;40mMasking-url")
+print("  # \033[1;34m[ 13 ] >> \033[1;36;40mAnon-SMS_Tool")
+print("  # \033[1;34m[ 14 ] >> \033[1;36;40mUpdate Utility
+print("  # \033[1;34m[ 15 ] >> \033[1;36;40mAbout Utility")
+print("  # \033[1;34m[ 16 ] >> \033[1;36;40mExit Utility")
 
 op=int(raw_input("Options: "))
 
 if(op==1):
- bash Default.sh
+ os.system("Python2 PhishingMenu.py")
 elif(op==2):
- bash Import.sh
+ os.system("Python2 SMSbomberMenu.py")
 elif(op==3):
+ os.system("WifiJummingMenu.py")
+elif(op==4):
+ os.system("WebDDOSMenu.py")
+elif(op==5):
+ os.system("WebAdminHackMenu.py")
+elif(op==6):
+ os.system("AndroidHackMenu.py")
+elif(op==7):
+ os.system("SQLinjectionMenu.py")
+elif(op==8):
+ os.system("SocialFinderMenu.py")
+elif(op==9):
+ os.system("SocialAttackMenu.py")
+elif(op==10):
+ os.system("DarkSearchMenu.py")
+elif(op==11):
+  os.system("MailHackMenu.py")
+elif(op==12:
+ os.system("MaskingUrlMenu.py")
+elif(op==13):
+ os.system("AnonSMSMenu.py")
+elif(op==14):
+
+elif(op==15):
+ os.system("python2 About.py")
+elif(op==16):
  print("\033[1;31;40mQuiting utility...")
  time.sleep(1)
  sys.exit()
