1db336e952e96d508a47ad382a9a888ebf6f232e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-24 11:33:50 +0300

    Create RouterFiles.sh

---
+++ Files/RouterFiles.sh
@@ -0,0 +1,31 @@
+apt-get install git
+apt-get install python
+apt-get install future
+apt-get install paramiko
+apt-get install pysnmp
+apt-get install pycrypto
+cd
+cd AllHackingTools
+apt-get install python3-pip
+git clone https://www.github.com/threat9/routersploit
+cd routersploit
+python3 -m pip install -r requirements.txt
+cd
+cd AllHackingTools
+git clone https://github.com/The404Hacking/websploit.git
+cd websploit/Setup && chmod +x install.sh && bash install.shcd
+cd commix 
+bash setup.sh
+cd
+cd AllHackingTools
+git clone https://github.com/AbirHasan2005/ShellPhish
+cd ShellPhish 
+chmod +x * 
+cd 
+cd AllHackingTools 
+cd Files
+sleep 1
+python2 PhishingMenu.py
+cd 
+cd AllHackingTools
+
