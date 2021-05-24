ec2eaccbfe5a304c137410723dff086de0a98a52
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-24 21:44:03 +0300

    Update AllHackingTool.sh

--- AllHackingTool.sh
+++ AllHackingTool.sh
@@ -13,25 +13,25 @@ echo "Please press enter to launch..."
 read a1
 echo -e $b">"$w" verify modules: "$g"python2"$w
 apt-get install python
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo -e $b">"$w" verify modules: "$g"pip3"$w
 apt-get install pip3
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo -e $b">"$w" verify modules: "$g"requests"$w
 pip3 install requests
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo -e $b">"$w" verify modules: "$g"php"$w
 apt-get install php
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo -e $b">"$w" verify modules: "$g"bs4"$w
 apt-get install bs4
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo -e $b">"$w" verify modules: "$g"wget"$w
 apt-get install wget
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo -e $b">"$w" verify modules: "$g"openssh"$w
 apt-get install openssh
-echo -e $b"[ ✔ ]"$g"succes"$w
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 echo "Succesfull verifined!"
 clear
 bash Logo.sh
