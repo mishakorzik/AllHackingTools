71a5562e8a87a0cc4678aa0eb874ec14dc97cd9c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 18:19:17 +0300

    Update AnimationLoad1.sh

--- src/AnimationLoad1.sh
+++ src/AnimationLoad1.sh
@@ -3,102 +3,102 @@ printf "\e[1;92m"
 
 printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.9
 clear
 printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ..\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading .\n"
-sleep 0.05
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ..\n"
-sleep 0.03
+sleep 0.09
 clear
 printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
-sleep 0.05
+sleep 0.09
 clear
