5205fd74d8d7521e6dc99677f93398c257e4e484
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 09:52:18 +0300

    Update RouterMenu.py

--- Files/RouterMenu.py
+++ Files/RouterMenu.py
@@ -16,10 +16,13 @@ print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit")
 op=int(raw_input("RouterHack: "))
 
 if(op==1):
+ os.system("clear")
  os.system("cd && cd AllHackingTools && cd routersploit && python3 rsf.py")
 elif(op==2):
+ os.system("clear")
  os.system("cd && cd AllHackingTools && cd websploit && sudo websploit")
 elif(op==3):
+ os.system("clear")
  os.system("cd && cd AllHackingTools && cd Commix && sudo python commix.py --wizard")
 elif(op==4):
  time.sleep(0.2)
