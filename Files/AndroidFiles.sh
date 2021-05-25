d9f558c65b7bd9b25d289a3f2067fc2dc110fb7b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 11:43:59 +0300

    Create AndroidFiles.sh

---
+++ Files/AndroidFiles.sh
@@ -0,0 +1,15 @@
+apt-get install git
+apt-get install python
+cd
+cd AllHackingTools
+git clone https://github.com/khalednassar702/Pyshell
+cd Pyshell
+cd
+cd AllHackingTools
+git clone https://github.com/kinghacker0/WishFish
+cd wishfish
+cd 
+cd AllHackingTools 
+cd Files
+cd 
+cd AllHackingTools
