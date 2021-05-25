39f56e66cecb03f34aef51f9382adbb20dc0ec5b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 10:38:24 +0300

    Create AnimationLoad1.py

---
+++ src/AnimationLoad1.py
@@ -0,0 +1,104 @@
+clear
+printf "\e[1;92m"
+
+printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.05
+clear
+printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ..\n"
+sleep 0.03
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
+sleep 0.05
+clear
