9fe047f2c8bbcc2dce4734763ee475c8082c4dc9
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 22:38:31 +0300

    Update PhishingMenu.py

--- Files/PhishingMenu.py
+++ Files/PhishingMenu.py
@@ -12,6 +12,7 @@ print("  # \033[1;34m[ 1 ] >> \033[1;36;40mShark")
 print("  # \033[1;34m[ 2 ] >> \033[1;36;40mZphisher")
 print("  # \033[1;34m[ 3 ] >> \033[1;36;40mShellphish")
 print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 5 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Phishing: "))
 
@@ -28,4 +29,8 @@ elif(op==4):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
+elif(op==5):
+ os.system("cd")
+ os.system("cd AllHackingTool")
+ os.system("python2 MainMenu.py")
  
