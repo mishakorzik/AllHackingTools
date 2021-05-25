234fc8a7eb5314d5ecd07630fc559133e39bc2f5
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 09:29:32 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,13 +1,24 @@
-echo -e "${WHITE}              [1] Termux "
-echo -e "${WHITE}              [0] Exit "
-echo -n -e "НаСK1NG >> "
-read choice
-INSTALL_DIR="/usr/share/doc/Tools-Hacking"
-BIN_DIR="/usr/bin/"
-if [ $choice == 1 ]; then 
-	echo "[*] Checking Internet Connection .."
-	wget -q --tries=10 --timeout=20 --spider https://google.com
-	if [[ $? -eq 0 ]]; then
-        cd
-        cd AllHackingTools
-        bash Files/Modules.sh
+clear
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+echo "╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╱╱╱╱╱╭━━━━╮╱╱╭━━━╮╭╮╱╱╱╱╱╭╮"
+echo "┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱┃╭━━╯╱╭╯╰╮╱╱╱╱╱┃╭╮╭╮┃╱╱┃╭━╮┣╯╰╮╱╱╱╭╯╰╮"
+echo "┃╰━╯┣━┳━━┳━━┳━━╮┃╰━━┳━╋╮╭╋━━┳━╮╰╯┃┃┣┻━╮┃╰━━╋╮╭╋━━┳┻╮╭╯"
+echo "┃╭━━┫╭┫┃━┫━━┫━━┫┃╭━━┫╭╮┫┃┃┃━┫╭╯╱╱┃┃┃╭╮┃╰━━╮┃┃┃┃╭╮┃╭┫┃"
+echo "┃┃╱╱┃┃┃┃━╋━━┣━━┃┃╰━━┫┃┃┃╰┫┃━┫┃╱╱╱┃┃┃╰╯┃┃╰━╯┃┃╰┫╭╮┃┃┃╰╮"
+echo "╰╯╱╱╰╯╰━━┻━━┻━━╯╰━━━┻╯╰┻━┻━━┻╯╱╱╱╰╯╰━━╯╰━━━╯╰━┻╯╰┻╯╰━╯"
+echo "Please press enter to start installing tool..."
+read a1
+echo -e $b"PLEASE A DON'T CLOSE TERMUX APP!"$w
+sleep 1
+echo "[*] Checking Internet Connection .."
+wget -q --tries=10 --timeout=20 --spider https://google.com
+if [[ $? -eq 0 ]]; then
+    sleep 1
+    echo -e $b"[ ✔ ]"$g"succesfull connected!"$w
+    sleep 0.5
+    cd
+    cd AllHackingTools
+    bash Files/Modules.sh
