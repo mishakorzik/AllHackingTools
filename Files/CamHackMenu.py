b0af3d984f0878296da862020f2022d5c45ff585
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 22:35:38 +0300

    Create CamHackMenu.py

---
+++ Files/CamHackMenu.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+import random
+import sys
+
+os.system("clear")
+os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+
+print("  # \033[1;34m[ 1 ] >> \033[1;36;40mCam-Hacker")
+print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGrabCam")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
+
+op=int(raw_input("Phishing: "))
+
+if(op==1):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Cam-Hackers && python2 cam-hackers.py")
+elif(op==2):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd grabcam && bash grabcam.sh")
+elif(op==3):
+ time.sleep(0.2)
+ print("\033[1;31;40mQuiting utility...")
+ sys.exit()
+elif(op==4):
+ os.system("cd")
+ os.system("cd AllHackingTool")
+ os.system("python2 MainMenu.py")
