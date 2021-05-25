a4baff9b98926d8d0de929ffd617b75ac6aa40ce
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 18:49:19 +0300

    Create SQLinjectionMenu.py

---
+++ Files/SQLinjectionMenu.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+import random
+import sys
+
+os.system("clear")
+os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+
+print("  # \033[1;34m[ 1 ] >> \033[1;36;40mVirusInjection")
+print("  # \033[1;34m[ 2 ] >> \033[1;36;40mMailHack")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit")
+
+op=int(raw_input("Iject1onTo0l: "))
+
+if(op==1):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Vitus2.0 && cp VirtualXposed.apk /sdcard/")
+ print("Your virus has been created!")
+ time.sleep 1
+ print("Find Your virus in /sdcard/")
+elif(op==2):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Debinject && python debinject.py")
+elif(op==3):
+ time.sleep(0.2)
+ print("\033[1;31;40mQuiting utility...")
+ sys.exit()
