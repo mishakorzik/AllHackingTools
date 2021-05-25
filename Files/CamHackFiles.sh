e151b7fe1ddcc6b17094a53a18cdc6e233d70115
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 22:28:56 +0300

    Create CamHackFiles.sh

---
+++ Files/CamHackFiles.sh
@@ -0,0 +1,19 @@
+apt-get install git
+apt-get install python
+cd
+cd AllHackingTools
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+pip3 install requests
+pip3 install colorama
+cd Cam-Hackers
+cd
+cd AllHackingTools
+git clone https://github.com/noob-hackers/grabcam
+cd 
+ls
+cd grabcam
+cd 
+cd AllHackingTools 
+cd Files
+cd 
+cd AllHackingTools
