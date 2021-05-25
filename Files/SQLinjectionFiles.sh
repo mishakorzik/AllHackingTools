e105ae669d28b029e813a58ecf36b44e9e10052a
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 18:37:00 +0300

    Create SQLinjectionFiles.sh

---
+++ Files/SQLinjectionFiles.sh
@@ -0,0 +1,16 @@
+apt-get install git
+apt-get install python
+cd
+git clone https://github.com/UndeadSec/Debinject.git
+cd Debinject
+cd
+cd AllHackingTools
+git clone git://github.com/htr-tech/zphisher.git
+cd 
+apt-get install python 2.x
+apt-get install dpkg
+apt-get install dpkg-deb
+cd AllHackingTools 
+cd Files
+cd 
+cd AllHackingTools
