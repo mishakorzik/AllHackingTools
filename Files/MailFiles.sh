9d6a84ccf524b20b7c8075772bdfe7e346b655c1
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 10:24:24 +0300

    Create MailFiles.sh

---
+++ Files/MailFiles.sh
@@ -0,0 +1,17 @@
+apt-get install git
+apt-get install python
+apt-get install tsu
+cd
+cd AllHackingTools
+git clone https://github.com/d4az/hack-gmail.git
+cd
+cd AllHackingTools
+git clone https://github.com/4w4k3/KnockMail.git
+cd KnockMail
+sudo su
+pip install -r requeriments.txt
+cd 
+cd AllHackingTools 
+cd Files
+cd 
+cd AllHackingTools
