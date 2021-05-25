506295fbec5239bb7aa04c924a1961bb25b2fe38
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-25 11:04:43 +0300

    Create WebFiles.sh

---
+++ Files/WebFiles.sh
@@ -0,0 +1,22 @@
+apt-get install git
+apt-get install python
+cd
+cd AllHackingTools
+git clone https://github.com/mishakorzik/AdminHack
+cd AdminHack
+bash setup.sh
+cd
+cd AllHackingTools
+git clone https://github.com/m4ll0k/takeover.git
+cd takeover
+python3 setup.py install
+cd
+cd AllHackingTools
+git clone https://github.com/UltimateHackers/Blazy
+cd Blazy
+pip install -r requirements.txt
+cd 
+cd AllHackingTools 
+cd Files
+cd 
+cd AllHackingTools
