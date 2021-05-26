b409594f0f1b3114e6407a931f9ad22f5de2dca7
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-26 10:34:43 +0300

    Create SnamFiles.sh

---
+++ Files/SnamFiles.sh
@@ -0,0 +1,26 @@
+apt-get install git
+apt-get install python
+apt-get install future
+apt-get install paramiko
+apt-get install pysnmp
+apt-get install pycrypto
+cd
+cd AllHackingTools
+pip install -r requirements.txt
+git clone https://github.com/Ivan-Hacker-700/SMS-Bomber-300-Free
+cd SMS-Bomber-300-Free
+cd
+cd AllHackingTools
+git clone https://github.com/anubhavanonymous/XLR8_BOMBER
+cd
+cd AllHackingTools
+git clone https://github.com/MaksPV/AresBomb
+cd 
+cd AllHackingTools
+git clone https://github.com/HACK3RY2J/Anon-SMS.git
+cd Anon-SMS
+cd
+cd AllHackingTools 
+cd Files
+cd 
+cd AllHackingTools
