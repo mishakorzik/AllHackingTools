465b36f8b73bdd6e0d14a4dcef58f6b6ca67bc38
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-27 11:40:13 +0300

    Create DarkSearchMenu.py

---
+++ Files/DarkSearchMenu.py
@@ -0,0 +1,18 @@
+apt-get install git
+apt-get install python
+apt-get install npm
+cd
+cd AllHackingTools
+git clone https://github.com/Bhaviktutorials/shark
+cd shark chmod +x *
+./setup
+cd
+cd AllHackingTools
+git clone git://github.com/htr-tech/zphisher.git
+cd
+cd AllHackingTools
+git clone https://github.com/josh0xA/darkdump
+cd darkdump
+python3 -m pip install -r requirements.txt
+cd 
+cd AllHackingTools
