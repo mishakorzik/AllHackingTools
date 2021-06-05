ed4f97350adef6e51ce38347100a112f99fff2ba
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-05 08:47:04 +0300

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -12,6 +12,6 @@ echo -e $o"██║░░██║██║░░██║╚█████╔
 echo -e $o"╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v1.2"
 echo -e ""
 echo -e " $g # Developer : mishakorzhik (DeplayGo)  $r     ATTENTION!  The author of this article is not respon- $o"
-echo -e " $g # version : AllHackingTool 1.1    $r  sible responsible for any consequences of reading it. All $o"
+echo -e " $g # version : AllHackingTool 1.2    $r  sible responsible for any consequences of reading it. All $o"
 echo -e " $g # Code  : bash, python      $r  materials are provided for educational purposes only! $o"
 echo -e ""
