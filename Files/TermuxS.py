fe293392c1e352c9a349b8fc7f2a9425c3b31888
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-05 17:57:36 +0300

    Create TermuxS.py

---
+++ Files/TermuxS.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+import random
+import sys
+
+os.system("clear")
+os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+
+print("  # \033[1;34m[ 1 ] >> \033[1;36;40mView MySystem")
+print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGoogle Search")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
+
+op=int(raw_input("TermUxPane1: "))
+
+if(op==1):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && bash Files/ViewS.sh")
+elif(op==2):
+ os.system("cd")
+ os.system("cd AllHackingTools")
+ os.system("w3m http://google.com/")
+elif(op==3):
+ time.sleep(0.2)
+ print("\033[1;31;40mQuiting utility...")
+ sys.exit()
+elif(op==4):
+ os.system("cd")
+ os.system("cd AllHackingTool")
+ os.system("python2 MainMenu.py")
