49b9608cc77db9290d2aa1dfece3cd4902d252e4
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-05 08:44:57 +0300

    Update Timer.py

--- src/Timer.py
+++ src/Timer.py
@@ -9,7 +9,7 @@ def animate():
     for c in itertools.cycle(['11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1']):
         if done:
             break
-        sys.stdout.write('\rAbout has close in seconds. ' + c)
+        sys.stdout.write('\rWindow has close in seconds. ' + c)
         sys.stdout.flush()
         time.sleep(1)
     sys.stdout.write('')
@@ -18,6 +18,6 @@ t = threading.Thread(target=animate)
 t.start()
 
  
-time.sleep(10.8)
+time.sleep(10.9)
 done = True
 print("")
