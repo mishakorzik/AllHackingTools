140f5e342a563698018feab28ebe06eddb621c40
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 10:32:55 +0300

    Create MailMenu.py

--- Files/MailMenu.py
+++ Files/MailMenu.py
@@ -8,9 +8,9 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  # \033[1;34m[ 1 ] >> \033[1;36;40mKnock")
-print("  # \033[1;34m[ 2 ] >> \033[1;36;40mMailHack")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 1 ] >> \033[1;36;40mKnock - Is subdomain Scaner")
+print("  # \033[1;34m[ 2 ] >> \033[1;36;40mMailHack - Email hacker")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit Utility")
 print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Ma1lHacK: "))
