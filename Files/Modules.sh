af24172834a8edd7db927a2c5964967e36f9be1c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 21:06:58 +0300

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -69,6 +69,7 @@ bash Files/PhishingFiles.sh
 bash Files/RouterFiles.sh
 bash Files/SQLinjectionFiles.sh
 bash Files/SpamFiles.sh
+bash IpFiles.sh
 sleep 1
 echo ██████╗░░█████╗░███╗░░██╗███████╗
 echo ██╔══██╗██╔══██╗████╗░██║██╔════╝
