a7208cb7edbcd60bb740726285b0d2822ae3709c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 10:36:26 +0300

    Update CamHackMenu.py

--- Files/CamHackMenu.py
+++ Files/CamHackMenu.py
@@ -8,9 +8,9 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  # \033[1;34m[ 1 ] >> \033[1;36;40mCam-Hacker")
-print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGrabCam")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 1 ] >> \033[1;36;40mCam-Hacker - Hack Cameras CCTV FREE")
+print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGrabCam - A tool to hack camera from termux")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit Utility")
 print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("CamPh1sh: "))
