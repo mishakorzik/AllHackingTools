b1bb7da66e18b911d964b68cb2d718e6818f6397
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 09:50:34 +0300

    Create Panels.sh

---
+++ Tool/Panels.sh
@@ -0,0 +1,9 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+echo -e $w"["$o"WARN"$w"]"$b"This tool version 1.3 update is not avaible!"$w
+sleep 0.5
+echo -e $w"["$g"INFO"$w"]"$b"Tranks for installing my tools! Trank you."$w
