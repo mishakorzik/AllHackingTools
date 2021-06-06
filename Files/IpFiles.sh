6347406cd2039a7f723def459fb7c769eb62618c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 22:10:33 +0300

    Update IpFiles.sh

--- Files/IpFiles.sh
+++ Files/IpFiles.sh
@@ -3,13 +3,6 @@ cd AllHackingTools
 git clone https://github.com/UndeadSec/EvilURL.git
 cd EvilURL
 cd
-cd
-cd AllHackingTools
-git clone https://github.com/joker25000/Devploit
-cd Devploit
-chmod +x install
-./install
-cd
 cd AllHackingTools
 git clone https://github.com/ciku370/OSIF
 cd OSIF
