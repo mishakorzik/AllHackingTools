7f2e24d8f8579be341a2a0f68b1e875b7ba60e4d
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 21:41:14 +0300

    Update and rename other.sh to Other.sh

---
+++ Files/Other.sh
@@ -0,0 +1,12 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+echo -e $w"["$o"WARNING"$w"]"$b"Error starting tool. Error code 53-73"$w
+sleep 0.5
+echo -e $w"["$g"ERROR"$w"]"$b"Critical Error to start tool. Please wait a new update!"$w
+sleep 0.7
+echo -e $w"["$o"INFO"$w"]"$b"Error code 53-73. Reloading AllHackingTools"$w
+sleep 1.4
+
