92ceee842ea8f3ca21416d14840970f3c1aa05f8
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 19:11:14 +0300

    Update CamHackMenu.py

--- Files/CamHackMenu.py
+++ Files/CamHackMenu.py
@@ -10,8 +10,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  # \033[1;34m[ 1 ] >> \033[1;36;40mCam-Hacker - Hack Cameras CCTV FREE")
 print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGrabCam - A tool to hack camera from termux")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit Utility")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mCamHack - hack Front Camera by sending a LINK")
+print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit Utility")
+print("  # \033[1;34m[ 5 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("CamPh1sh: "))
 
@@ -22,10 +23,13 @@ elif(op==2):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd grabcam && bash grabcam.sh")
 elif(op==3):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd CamHack && bash bash CamHack.sh")
+elif(op==4):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==4):
+elif(op==5):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
