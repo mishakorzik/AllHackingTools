c3f7b15db3f125ee93a3208046ee608902cf992f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 09:32:51 +0300

    Update WebMenu.py

--- Files/WebMenu.py
+++ Files/WebMenu.py
@@ -11,8 +11,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 print("  # \033[1;34m[ 1 ] >> \033[1;36;40mAdminHack")
 print("  # \033[1;34m[ 2 ] >> \033[1;36;40mSubDom")
 print("  # \033[1;34m[ 3 ] >> \033[1;36;40mBlazy")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit")
-print("  # \033[1;34m[ 5 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 4 ] >> \033[1;36;40mSqlMap")
+print("  # \033[1;34m[ 5 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 6 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Web-Hack1Ng: "))
 
@@ -26,10 +27,13 @@ elif(op==3):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd Blazy && python blazy.py")
 elif(op==4):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd sqlmap-dev && python sqlmap.py -wizard")
+elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==5):
+elif(op==6):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
