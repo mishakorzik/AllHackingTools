bace5768f0ba9eff9037e533b9eedd23ee3ea65d
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 21:44:25 +0300

    Update Other.sh

--- Files/Other.sh
+++ Files/Other.sh
@@ -9,4 +9,13 @@ echo -e $w"["$g"ERROR"$w"]"$b"Critical Error to start tool. Please wait a new up
 sleep 0.7
 echo -e $w"["$o"INFO"$w"]"$b"Error code 53-73. Reloading AllHackingTools"$w
 sleep 1.4
+echo -e $w"["$g"ERROR"$w"]"$b"Error to reloading tool"$w
+echo -e $w"["$o"WARNING"$w"]"$b"All Reseting Tool"$w
+sleep 1.4
+cd
+cd
+cd AllHackingTools 
+bash AllHackingTool.sh
+
+
 
