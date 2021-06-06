c5869af21ed9be4937012d335cbc2a352bdde7f0
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 21:05:01 +0300

    Create IpFiles.sh

---
+++ Files/IpFiles.sh
@@ -0,0 +1,19 @@
+cd
+cd AllHackingTools
+git clone https://github.com/UndeadSec/EvilURL.git
+cd EvilURL
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/joker25000/Devploit
+cd Devploit
+chmod +x install
+./install
+cd
+cd AllHackingTools
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+cd
+cd
+cd AllHackingTools
