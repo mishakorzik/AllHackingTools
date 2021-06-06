59243837c63a6188f428147c6dcc442e42463b1c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 21:06:11 +0300

    Update InformationT.py

--- Files/InformationT.py
+++ Files/InformationT.py
@@ -8,23 +8,23 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  # \033[1;34m[ 1 ] >> \033[1;36;40mCam-Hacker - Hack Cameras CCTV FREE")
-print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGrabCam - A tool to hack camera from termux")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mCamHack - hack Front Camera by sending a LINK")
+print("  # \033[1;34m[ 1 ] >> \033[1;36;40mEvilURL - Generate unicode evil domains for IDN Homograph Attack and detect them")
+print("  # \033[1;34m[ 2 ] >> \033[1;36;40mDevploit - Simple Information Gathering Tool")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mOSIF - Open Source Information Facebook")
 print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit Utility")
 print("  # \033[1;34m[ 5 ] >> \033[1;36;40mBack To MainMenu")
 
-op=int(raw_input("CamPh1sh: "))
+op=int(raw_input("1pAdressHaCK: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Cam-Hackers && python3 cam-hackers.py")
+ os.system("cd && cd AllHackingTools && cd EvilURL && python3 evilurl.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd grabcam && bash grabcam.sh")
+ os.system("cd && cd AllHackingTools && cd Devploit && Devploit")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd CamHack && bash bash CamHack.sh")
+ os.system("cd && cd AllHackingTools && cd OSIF && python2 osif.py")
 elif(op==4):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
