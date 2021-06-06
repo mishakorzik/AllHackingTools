236bc358ad72b507ac0b1bcadf8e8ab913ef0622
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 22:21:10 +0300

    Update Inf.sh

--- src/Inf.sh
+++ src/Inf.sh
@@ -5,3 +5,5 @@ w="\033[0m"
 o="\033[1;33m"
 
 echo -e $w"["$g"INFO"$w"]"$b"Starting tool menu. Please wait a moment!"$w
+sleep 0.1
+echo -e $w"["$o"WARN"$w"]"$b"A don't close termux app! And exit termux!"$w
