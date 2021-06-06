ab8ce65869026bd09e533891429ab8212a413b20
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-06 22:21:57 +0300

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -42,64 +42,64 @@ op=int(raw_input("Options: "))
 
 if(op==1):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/PhishingMenu.py")
 elif(op==2):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/RouterMenu.py")
 elif(op==3):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/MailMenu.py")
 elif(op==4):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/WebMenu.py")
 elif(op==5):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/CamHackMenu.py")
 elif(op==6):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/AndroidMenu.py")
 elif(op==7):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/SQLinjectionMenu.py")
 elif(op==8):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/SocialMenu.py")
 elif(op==9):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/SpamMenu.py")
 elif(op==10):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/DarkSearchMenu.py")
 elif(op==11):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/IpMenu.py")
 elif(op==12):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("bash Files/Other.sh")
 elif(op==13):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("python2 Files/TermuxS.py")
 elif(op==14):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  time.sleep(1)
  os.system("bash Files/Updater.sh")
 elif(op==15):
  os.system("bash src/Inf.sh")
- time.sleep(0.4)
+ time.sleep(0.3)
  os.system("bash src/About.sh")
 elif(op==16):
  print("\033[1;31;40mQuiting utility...")
