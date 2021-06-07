92c7050ecd5d495d2f99ba1b75d421738f7bfeec
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-07 16:05:26 +0300

    Update WebMenu.py

--- Files/WebMenu.py
+++ Files/WebMenu.py
@@ -13,9 +13,10 @@ print("  # \033[1;34m[ 2 ] >> \033[1;36;40mSubDom - cname finder tool")
 print("  # \033[1;34m[ 3 ] >> \033[1;36;40mBlazy - modern login bruteforcer")
 print("  # \033[1;34m[ 4 ] >> \033[1;36;40mSqlMap - SQL injection and database takeover tool")
 print("  # \033[1;34m[ 5 ] >> \033[1;36;40mWebSploit - An advanced MiTM Framework")
-print("  # \033[1;34m[ 6 ] >> \033[1;36;40mSH33LL - Shell scanner")
-print("  # \033[1;34m[ 7 ] >> \033[1;36;40mExit Utility")
-print("  # \033[1;34m[ 8 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 6 ] >> \033[1;36;40mSqlmate - A friend of SQLmap which will do what you always expected from SQLmap")
+print("  # \033[1;34m[ 7 ] >> \033[1;36;40mSH33LL - Shell scanner")
+print("  # \033[1;34m[ 8 ] >> \033[1;36;40mExit Utility")
+print("  # \033[1;34m[ 9 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Web-Hack1Ng: "))
 
@@ -36,12 +37,15 @@ elif(op==5):
  os.system("cd && cd AllHackingTools && cd websploit && websploit")
 elif(op==6):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd SH33LL && python sh33l.py")
+ os.system("cd && cd AllHackingTools && cd sqlmate && python sqlmate")
 elif(op==7):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd SH33LL && python sh33l.py")
+elif(op==8):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==8):
+elif(op==9):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
