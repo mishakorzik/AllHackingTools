0c07de61ca1b112ecb9d12e96f7a19ac7b44bd56
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-07 22:55:56 +0300

    Create PassworldFiles.sh

---
+++ Files/PassworldFiles.sh
@@ -0,0 +1,21 @@
+cd
+cd AllHackingTools
+git clone https://github.com/ciku370/hasher
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/thamahaxor/hasherdoid
+cd hasherdoid
+chmod +x hasherdotid.py
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/ciku370/hash-generator
+cd hash-generator
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/s0md3v/Hash-Buster
+cd
+cd
+cd AllHackingTools
