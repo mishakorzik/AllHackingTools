405bd78d50553166c866d5c671feda9420377e95
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 15:52:25 +0300

    Update SpamMenu.py

--- Files/SpamMenu.py
+++ Files/SpamMenu.py
@@ -10,9 +10,10 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 print("  # \033[1;34m[ 01 ] >> \033[1;36;40mSMS-Bomber-300 - ultra bomber for 300 services")
 print("  # \033[1;34m[ 02 ] >> \033[1;36;40mXLR8_BOMBER - hard bomber & cals")
 print("  # \033[1;34m[ 03 ] >> \033[1;36;40mAnon-SMS - Send Messages Anonymously")
-print("  # \033[1;34m[ 04 ] >> \033[1;36;40mAres-Bomb - medium bomber")
-print("  # \033[1;34m[ 05 ] >> \033[1;36;40mExit Utility")
-print("  # \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 04 ] >> \033[1;36;40mSpymer - more options sms and cals bomber")
+print("  # \033[1;34m[ 05 ] >> \033[1;36;40mAres-Bomb - medium bomber")
+print("  # \033[1;34m[ 06 ] >> \033[1;36;40mExit Utility")
+print("  # \033[1;34m[ 07 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("SpymER: "))
 
@@ -27,12 +28,15 @@ elif(op==3):
  os.system("cd && cd AllHackingTools && cd Anon-SMS && bash Run.sh")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd AresBomb && python boom.py")
+ os.system("cd && cd AllHackingTools && spymer")
 elif(op==5):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd AresBomb && python boom.py")
+elif(op==6):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==6):
+elif(op==7):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
