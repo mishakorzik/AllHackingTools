5a45a9c3146bab1322e62381cfd19bec33cb7dd8
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 23:04:43 +0300

    Update SubDom.py

--- Files/SubDom.py
+++ Files/SubDom.py
@@ -4,6 +4,6 @@ os.system("cd AllHackingTools")
 os.system("bash Logo.sh")
 os.system("cd takeover")
 site=int(raw_input("\033[1;33;40mEnter you url Site :"))
-python3 takeover.py -d site -v 
+python3 takeover.py -d '+site+' -v 
 os.system("cd")
 os.system("cd AllHackingTools")
