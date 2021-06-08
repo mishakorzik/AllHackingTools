3fc0bcd63325ed3431661e38be02a257e55ca989
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 15:01:19 +0300

    Update PassworldMenu.py

--- Files/PassworldMenu.py
+++ Files/PassworldMenu.py
@@ -7,12 +7,12 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  # \033[1;34m[ 1 ] >> \033[1;36;40mHasher - Hash cracker with auto detect hash")
-print("  # \033[1;34m[ 2 ] >> \033[1;36;40mHasherDoid - A tool for find an encrypted text")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mHash Generator - Beautiful Hash Generator")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mHash Buster - Crack hashes in seconds")
-print("  # \033[1;34m[ 5 ] >> \033[1;36;40mExit Utility")
-print("  # \033[1;34m[ 6 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 01 ] >> \033[1;36;40mHasher - Hash cracker with auto detect hash")
+print("  # \033[1;34m[ 02 ] >> \033[1;36;40mHasherDoid - A tool for find an encrypted text")
+print("  # \033[1;34m[ 03 ] >> \033[1;36;40mHash Generator - Beautiful Hash Generator")
+print("  # \033[1;34m[ 04 ] >> \033[1;36;40mHash Buster - Crack hashes in seconds")
+print("  # \033[1;34m[ 05 ] >> \033[1;36;40mExit Utility")
+print("  # \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Ma1lHacK: "))
 
