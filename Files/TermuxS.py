9d126db78db36942dea1ea635fc271e59204ca3a
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 15:38:56 +0300

    Update TermuxS.py

--- Files/TermuxS.py
+++ Files/TermuxS.py
@@ -7,13 +7,14 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  # \033[1;34m[ 1 ] >> \033[1;36;40mView MySystem")
-print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGoogle Search")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mPassGenerator")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mTermux Style")
-print("  # \033[1;34m[ 5 ] >> \033[1;36;40mClear logs")
-print("  # \033[1;34m[ 6 ] >> \033[1;36;40mExit system")
-print("  # \033[1;34m[ 7 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 01 ] >> \033[1;36;40mView MySystem")
+print("  # \033[1;34m[ 02 ] >> \033[1;36;40mGoogle Search")
+print("  # \033[1;34m[ 03 ] >> \033[1;36;40mBattarey Status")
+print("  # \033[1;34m[ 04 ] >> \033[1;36;40mPassGenerator")
+print("  # \033[1;34m[ 05 ] >> \033[1;36;40mTermux Style")
+print("  # \033[1;34m[ 06 ] >> \033[1;36;40mClear logs")
+print("  # \033[1;34m[ 07 ] >> \033[1;36;40mExit system")
+print("  # \033[1;34m[ 08 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("TermUxPane1: "))
 
@@ -25,25 +26,29 @@ elif(op==2):
  os.system("cd AllHackingTools")
  os.system("w3m http://google.com/")
 elif(op==3):
- os.system("cd")
- os.system("cd AllHackingTools")
- os.system("bash src/PassGenerator.sh")
+ os.system("cd && cd AllHackingTools")
+ os.system("termux-battery-status")
+ os.system("python2 src/aboutMenu.py")
 elif(op==4):
  os.system("cd")
  os.system("cd AllHackingTools")
- os.system("termux-style")
+ os.system("bash src/PassGenerator.sh")
 elif(op==5):
+ os.system("cd")
+ os.system("cd AllHackingTools")
+ os.system("termux-style")
+elif(op==6):
  os.system("cd")
  os.system("cd .. && cd usr/var/log/apt && rm -r history.log && rm -r term.log && rm -r eipp.log.xz && cd .. && rm -r alternatives.log")
  time.sleep(0.5)
  print("\033[1;31;40mTermux logs has been cleaned...")
  time.sleep(1)
  os.system("cd && cd AllHackingTools && python2 MainMenu.py")
-elif(op==6):
+elif(op==7):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==7):
+elif(op==8):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
