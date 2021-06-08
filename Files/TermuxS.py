dc221402462cc141e7dceec6ec2c6d06c5ebba4e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 11:11:30 +0300

    Update TermuxS.py

--- Files/TermuxS.py
+++ Files/TermuxS.py
@@ -10,8 +10,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 print("  # \033[1;34m[ 1 ] >> \033[1;36;40mView MySystem")
 print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGoogle Search")
 print("  # \033[1;34m[ 3 ] >> \033[1;36;40mPassGenerator")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit system")
-print("  # \033[1;34m[ 5 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 4 ] >> \033[1;36;40mTermux Style")
+print("  # \033[1;34m[ 5 ] >> \033[1;36;40mExit system")
+print("  # \033[1;34m[ 6 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("TermUxPane1: "))
 
@@ -27,10 +28,14 @@ elif(op==3):
  os.system("cd AllHackingTools")
  os.system("bash src/PassGenerator.sh")
 elif(op==4):
+ os.system("cd")
+ os.system("cd AllHackingTools")
+ os.system("termux-style")
+elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==5):
+elif(op==6):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
