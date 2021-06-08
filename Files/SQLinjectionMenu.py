74d8330f8f3c499543498b37d39c51c5504654e8
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 15:02:33 +0300

    Update SQLinjectionMenu.py

--- Files/SQLinjectionMenu.py
+++ Files/SQLinjectionMenu.py
@@ -7,10 +7,10 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  # \033[1;34m[ 1 ] >> \033[1;36;40mVirusInjection - create a virus in /sdcard/")
-print("  # \033[1;34m[ 2 ] >> \033[1;36;40mDebinject - Inject malicious code into *.debs")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit Utility")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 01 ] >> \033[1;36;40mVirusInjection - create a virus in /sdcard/")
+print("  # \033[1;34m[ 02 ] >> \033[1;36;40mDebinject - Inject malicious code into *.debs")
+print("  # \033[1;34m[ 03 ] >> \033[1;36;40mExit Utility")
+print("  # \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Iject1onTo0l: "))
 
