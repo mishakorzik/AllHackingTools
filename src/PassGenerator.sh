96d3d6106ea069fa6f4c7e3ab0683a9ebc98a135
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 10:39:42 +0300

    Update PassGenerator.sh

--- src/PassGenerator.sh
+++ src/PassGenerator.sh
@@ -1,3 +1,10 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+echo -e $w"["$g"INFO"$w"]"$b"Easy passworlds"$w
 echo 835fU930sgi52fGs
 echo 42jdU5BJ62w5283wW
 echo 724gIEN62o0wU
@@ -23,14 +30,13 @@ echo hReI7T372BsiywvJ2g
 echo jEwbU52vw4&I8Ns897B2
 echo uw42Bwo72eKdbW72EqN
 echo wub564o54HuGsi4HSr72
-echo 82DsycU4384jsT9365Gs2
-echo 83w4q24dQ3W9Gwt21Jwbo
-echo 73d4fsyR38gsbUsbU362Vj
-echo 8wu2BwoE3NO9E8JfW52B8
-echo I8725BdUqe72jIv41&5
-echo uwT27529hTEbne538
-echo he27Jey9262WrwH2021
-echo Hard Passworlds
+echo -e $w"["$g"INFO"$w"]"$b"Medium passworlds"$w
+echo 82DsycU4384jsT9365Gs282Vqr
+echo 83w4q24dQ3W9Gwt21JwboYwfc
+echo 2iegdjdi422GsjRw835NdoR
+echo I8725BdUq2644e72jIv41&5
+echo uwT27529hTEbne538734bD7Bw
+echo he27Jey9262WrwH20212ejdO
 echo 73625dvshRge428Garq4280Hvs
 echo 73bsuR623nfoheb573Gr52H73bdhRajI
 echo uw638B62nfi6Vwojdv8VeU734bdhh
@@ -45,3 +51,14 @@ echo jsc2730YInQrhO373814Odbod72
 echo Hsrs25134wjTbwk82359dhKdh50606
 echo hd19475eugFauj2783GyBAJAIEU3737
 echo dkey628DahwIo2628BTtfGb7252
+echo -e $w"["$g"INFO"$w"]"$b"Hard passworlds"$w
+echo 46hRRwn42eIuab7T3eue6383272BsiyOeishgIehd63B
+echo RYw52jEHvEwbUjsteInfoNqfe62852vw4&I8Ns8ifn97B2
+echo uw28ywhIe42Bwo72Ujwnjsgfz538eUhwbKdbW72EqN
+echo 82DsycU43wbbUshbw63ca84jsT936sigTsgb5Gs23
+echo 82jsb287sjjsgsvTenkd263638odhbTa94Ishb94
+echo Ruwbi13Rhsvsgwb1512Of2473nIqfBf2fRsv3i724
+echo jsjsVRRc2730YInQrhO3348273814Odbod72dhst
+echo hejwyv8352bsjsFr182awbdkdiB8263Vsj736Bd
+echo 33nshsrrwsjkfodugssqg833542jqjGwboTvw
+echo dkey628DahwIo2628BTtfGb7252BsyragUso62I
