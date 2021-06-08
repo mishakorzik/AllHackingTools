36f8f23c9e0ebbc03c0701dde8097d11f3bc82e2
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 08:59:39 +0300

    Update aboutMenu.py

--- src/aboutMenu.py
+++ src/aboutMenu.py
@@ -5,7 +5,8 @@ import random
 import sys
 
 os.system("clear")
-os.system("bash Logo.sh")
+os.system("cd && cd AllHackingTools")
+os.system("bash Logo1.sh")
 
 Green="\033[1;33m"
 Blue="\033[1;34m"
@@ -15,7 +16,8 @@ Red="\033[1;31m"
 Purple="\033[0;35m"
 
 print(" # \033[1;33m[ 01 ] >> \033[1;36;40mBack To MainMenu")
-print(" # \033[1;33m[ 02 ] >> \033[1;36;40mExit Utility")
+print(" # \033[1;33m[ 02 ] >> \033[1;36;40mReboot system")
+print(" # \033[1;33m[ 03 ] >> \033[1;36;40mExit Utility")
 
 op=int(raw_input("Y0uR D1ya: "))
 
@@ -23,10 +25,12 @@ if(op==1):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
-elif(op==2):
- print("\033[1;31;40mQuiting utility...")
+elif(op==3):
+ print("\033[1;31;40mRebooting...")
  time.sleep(1)
- sys.exit()
+ os.system("cd")
+ os.system("cd AllHackingTools")
+ os.system("bash AllHackingTool.sh")
 else:
  print("\033[1;31;40mInvalid input. Back To MainMenu...")
  time.sleep(1)
