73720dfe20d0bb931c1c24a0456baca8f35a3c6e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 10:47:38 +0300

    Update TermuxS.py

--- Files/TermuxS.py
+++ Files/TermuxS.py
@@ -9,8 +9,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  # \033[1;34m[ 1 ] >> \033[1;36;40mView MySystem")
 print("  # \033[1;34m[ 2 ] >> \033[1;36;40mGoogle Search")
-print("  # \033[1;34m[ 3 ] >> \033[1;36;40mExit")
-print("  # \033[1;34m[ 4 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 3 ] >> \033[1;36;40mPassGenerator")
+print("  # \033[1;34m[ 4 ] >> \033[1;36;40mExit")
+print("  # \033[1;34m[ 5 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("TermUxPane1: "))
 
@@ -22,10 +23,14 @@ elif(op==2):
  os.system("cd AllHackingTools")
  os.system("w3m http://google.com/")
 elif(op==3):
+ os.system("cd")
+ os.system("cd AllHackingTools")
+ os.system("bash src/PassGenerator.sh")
+elif(op==4):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==4):
+elif(op==5):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
