821c07a4644c989c18ce81ea3522381e410f4f39
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 09:41:53 +0300

    Update DarkSearchFiles.sh

--- Files/DarkSearchFiles.sh
+++ Files/DarkSearchFiles.sh
@@ -2,13 +2,6 @@ apt-get install git
 apt-get install python
 apt-get install npm
 cd
-cd AllHackingTools
-git clone https://github.com/Bhaviktutorials/shark
-cd shark chmod +x *
-./setup
-cd
-cd AllHackingTools
-git clone git://github.com/htr-tech/zphisher.git
 cd
 cd AllHackingTools
 git clone https://github.com/josh0xA/darkdump
