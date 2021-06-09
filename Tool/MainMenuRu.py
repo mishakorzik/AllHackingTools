40aea2cce0c3c5e8088744b4a5d327050646a145
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-09 22:07:45 +0300

    Update and rename AllHackingToolRU.sh to MainMenuRu.py

--- Tool/MainMenuRu.py
+++ Tool/MainMenuRu.py
@@ -43,81 +43,98 @@ print("  # \033[1;34m[ 18 ] >> \033[1;36;40mExit System:  -  log out system")
 op=int(raw_input("Options: "))
 
 if(op==1):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/IpMenu.py")
 elif(op==2):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/RouterMenu.py")
 elif(op==3):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/MailMenu.py")
 elif(op==4):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/WebMenu.py")
 elif(op==5):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/CamHackMenu.py")
 elif(op==6):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/AndroidMenu.py")
 elif(op==7):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/SQLinjectionMenu.py")
 elif(op==8):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/SocialMenu.py")
 elif(op==9):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/SpamMenu.py")
 elif(op==10):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/AnalistickMenu.py")
 elif(op==11):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/DarkSearchMenu.py")
 elif(op==12):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/PhishingMenu.py")
 elif(op==13):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/PassworldMenu.py")
 elif(op==14):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("bash src/InfErr.sh")
 elif(op==15):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("python2 Files/TermuxS.py")
 elif(op==16):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  time.sleep(1)
  os.system("bash Files/Updater.sh")
 elif(op==17):
+ os.system("cd && cd AllHackingTools")
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  os.system("bash src/About.sh")
 elif(op==18):
- print("\033[1;31;40mQuiting utility...")
+ print("\033[1;31;40mЗакритые програми...")
  time.sleep(1)
  sys.exit()
 else:
- print("\033[1;31;40mInvalid input. Reloading Tools") 
+ print("\033[1;31;40mНеверная опция. Перезагрузка системи") 
  time.sleep(1.6)
  os.system("cd")
  os.system("cd AllHackingTool")
- os.system("python2 MainMenu.py")
+ os.system("python2 Tool/MainMenuRu.py")
