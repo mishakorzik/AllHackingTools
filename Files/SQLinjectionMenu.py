b06eed0d02c2544d8f9c78bb45791ae4defe4f93
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-09 10:54:36 +0300

    Update SQLinjectionMenu.py

--- Files/SQLinjectionMenu.py
+++ Files/SQLinjectionMenu.py
@@ -9,8 +9,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  # \033[1;34m[ 01 ] >> \033[1;36;40mVirusInjection - create a virus in /sdcard/")
 print("  # \033[1;34m[ 02 ] >> \033[1;36;40mDebinject - Inject malicious code into *.debs")
-print("  # \033[1;34m[ 03 ] >> \033[1;36;40mExit Utility")
-print("  # \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu")
+print("  # \033[1;34m[ 03 ] >> \033[1;36;40mZvirus - Create a virus for windows, android, linux")
+print("  # \033[1;34m[ 04 ] >> \033[1;36;40mExit Utility")
+print("  # \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Iject1onTo0l: "))
 
@@ -24,10 +25,13 @@ elif(op==2):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd Debinject && python2 debinject.py")
 elif(op==3):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd zVirus-Gen && bash zVirus")
+elif(op==4):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==4):
+elif(op==5):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
