990f5a5eedb403e5cecc073937be1d626d84c3f7
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-10 17:57:45 +0300

    Update Timer2.py

--- src/Timer2.py
+++ src/Timer2.py
@@ -6,10 +6,10 @@ import sys
 done = False
 
 def animate():
-    for c in itertools.cycle(['33', '32', '31', '30', '29', '28', '27', '26', '25', '24', '23', '22', '21', '20', '19', '18', '17', '16', '15', '14', '13', '12', '11', '10', '09', '08', '07', '06', '05', '04', '03', '02', '01']):
+    for c in itertools.cycle(['24', '23', '22', '21', '20', '19', '18', '17', '16', '15', '14', '13', '12', '11', '10', '09', '08', '07', '06', '05', '04', '03', '02', '01']):
         if done:
             break
-        sys.stdout.write('\Launch the tool in a few seconds ' + c)
+        sys.stdout.write('\rWindow has close in seconds. ' + c)
         sys.stdout.flush()
         time.sleep(1)
     sys.stdout.write('')
@@ -18,6 +18,6 @@ t = threading.Thread(target=animate)
 t.start()
 
  
-time.sleep(32.9)
+time.sleep(23.9)
 done = True
 print("")
