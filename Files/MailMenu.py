1943e360a47b68e9f24a1226ddc2e5a9a775b099
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-10 18:16:01 +0300

    Update MailMenu.py

--- Files/MailMenu.py
+++ Files/MailMenu.py
@@ -24,3 +24,9 @@ elif(op==3):
  os.system("cd")
  os.system("cd AllHackingTool")
  os.system("python2 MainMenu.py")
+else:
+ print("\033[1;31;40mInvalid input. Reloading Tools") 
+ time.sleep(1.6)
+ os.system("cd")
+ os.system("cd AllHackingTool")
+ os.system("python2 Files/MailMenu.py")
