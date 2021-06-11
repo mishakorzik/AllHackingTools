1d7367a389bde82d277220bf60c5978b0447d357
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-11 23:34:35 +0300

    Update RouterMenu.py

--- Files/RouterMenu.py
+++ Files/RouterMenu.py
@@ -34,6 +34,12 @@ elif(op==5):
  sys.exit()
 elif(op==6):
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd AllHackingTools")
+ os.system("python2 MainMenu.py")
+else:
+ print("\033[1;31;40mInvalid input. Reloading Tools") 
+ time.sleep(1.6)
+ os.system("cd")
+ os.system("cd AllHackingTools")
  os.system("python2 Files/RouterMenu.py")
  
