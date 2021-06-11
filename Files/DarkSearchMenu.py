581dd8fd827f0a4aa2f80b79e01313913f813b0f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-11 23:34:21 +0300

    Update DarkSearchMenu.py

--- Files/DarkSearchMenu.py
+++ Files/DarkSearchMenu.py
@@ -22,7 +22,7 @@ elif(op==2):
  sys.exit()
 elif(op==3):
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
 else:
  print("\033[1;31;40mInvalid input. Reloading Tools") 
