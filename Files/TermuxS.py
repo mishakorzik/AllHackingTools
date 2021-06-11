b724905278fd154a56c25f1ccf2295e869951b1e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-11 23:40:56 +0300

    Update TermuxS.py

--- Files/TermuxS.py
+++ Files/TermuxS.py
@@ -63,11 +63,11 @@ elif(op==10):
  sys.exit()
 elif(op==11):
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
 else:
  print("\033[1;31;40mInvalid input. Reloading Tools") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd AllHackingTools")
  os.system("python2 Files/TermuxS.py")
