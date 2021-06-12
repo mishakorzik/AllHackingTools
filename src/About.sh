2de6863c016152909428fc93e145a1219a7b6ff0
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-12 18:19:42 +0300

    Update About.sh

--- src/About.sh
+++ src/About.sh
@@ -1,20 +1,20 @@
 
-echo -n ${BLUE}"
+echo '
     ┌──────────────────────────────────────────────────────────────┐
-    │  			     ┏━┓┏┓ ┏━┓╻ ╻╺┳╸			   │
+    │  	                     ┏━┓┏┓ ┏━┓╻ ╻╺┳╸			   │
     │  			     ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 			   │
     │  			     ╹ ╹┗━┛┗━┛┗━┛ ╹ 			   │
     │──────────────────────────────────────────────────────────────│
     │                                                              │
-    │ >>  'Hi, here you can see my telegram and github there       |
+    │ >>   Hi, here you can see my telegram and github there       |
     |       will be questions you will write in telegram.          │
     │                                                              │
     │ >>  Author   :  Misha Korzhik                                │
     │     Github   :  https://github.com/mishakorzik               │
     │     Telegram :  pseudonym @MishaKorzhikTelegram              │
     │                                                              │
-    └──────────────────────────────────────────────────────────────┘" 
-echo ""
+    └──────────────────────────────────────────────────────────────┘ 
+'|lolcat -a -d 10 -p 1.5 -s 40.0
 cd && cd AllHackingTools  
 python3 src/Timer.py
 python2 src/aboutMenu.py   
