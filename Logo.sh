dd3e6f602d761d7f9d2278302c44aeca28662c53
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-12 15:53:05 +0300

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -17,24 +17,3 @@ echo '
 echo -e " $g # Developer : mishakorzhik (DeplayGo)  $r     ATTENTION!  The author of this article is not respon- $o"
 echo -e " $g # version : AllHackingTool 1.5    $r  sible responsible for any consequences of reading it. All $o"
 echo -e " $g # Code  : bash, python      $r  materials are provided for educational purposes only! $bl"
-echo '
-  # [ 01 ] >> Information Gathering:  -  Collects information from database
-  # [ 02 ] >> Exploitation Tools:  -  selection of tools for operation and hacking
-  # [ 03 ] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
-  # [ 04 ] >> Web Hacking Tool:  -  Tools for hacking websites and servers
-  # [ 05 ] >> Cam Hacking Tool:  -  Tools for hacking cams and front camera
-  # [ 06 ] >> InjectionRAT Utilites:  -  Utilities for creating RAT virus
-  # [ 07 ] >> SQL injection-Tool:  -  Tools for creating viruses
-  # [ 08 ] >> SocialMedia Hacking:  -  Tools for hacking social networks
-  # [ 09 ] >> SMS-spaming Tools:  -  Utilities for spam and anonymous SMS
-  # [ 10 ] >> Vulnerability Analysis:  -  Systems for vulnerability analysis
-  # [ 11 ] >> DarkSearch Tool:  -  Tools for Secret and Onion search 
-  # [ 12 ] >> Phishing Tool:  -  Tools for phishing and fake websites
-  # [ 13 ] >> Passworld Attack:  -  Utilities for cracking passwords
-
-  # [ 14 ] >> Other tools:  -  Separate and other tools for hacking
-  # [ 15 ] >> Termux Panel:  -  Termux control and special features
-  # [ 16 ] >> Update System:  -  Update system and all tools & menu
-  # [ 17 ] >> About System:  -  View system information and developer
-  # [ 18 ] >> Exit System:  -  log out system
-'|lolcat -p 2.0
