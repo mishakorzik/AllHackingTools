a539fe24a2017c137b296da9d564798ae90bc666
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-12 18:26:02 +0300

    Create MenuA.sh

---
+++ src/MenuA.sh
@@ -0,0 +1,6 @@
+echo ' 
+ # [ 01 ] >> Back To MainMenu  -  Back to MainMenu 
+ # [ 02 ] >> Reboot system  -  Reboot All tools
+ # [ 03 ] >> Exit System  -  log out system
+'|lolcat -p 1.0 -a -d 10 -s 30.0
+
