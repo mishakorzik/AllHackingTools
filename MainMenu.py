edd1d7f5d8f54eb27c3d31a0b18b468761031b0c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-12 15:33:46 +0300

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 os.system("clear")
-os.system("bash Logo.sh")
+os.system("bash Logo2.sh")
 
 Green="\033[1;33m"
 Blue="\033[1;34m"
@@ -20,26 +20,6 @@ w="\033[0m"
 b="\033[1;34m"
 o="\033[1;33m"
 bl="\033[1;36;40m"
-
-print("  # \033[1;34m[ 01 ] >> \033[1;36;40mInformation Gathering:  -  Collects information from a different database")
-print("  # \033[1;34m[ 02 ] >> \033[1;36;40mExploitation Tools:  -  selection of tools for operation and hacking")
-print("  # \033[1;34m[ 03 ] >> \033[1;36;40mSniffing and Spoofing:  -  Tools for forgery of data and databases")
-print("  # \033[1;34m[ 04 ] >> \033[1;36;40mWeb Hacking Tool:  -  Tools for hacking websites and servers")
-print("  # \033[1;34m[ 05 ] >> \033[1;36;40mCam Hacking Tool:  -  Tools for hacking cams and front camera")
-print("  # \033[1;34m[ 06 ] >> \033[1;36;40mInjectionRAT Utilites:  -  Utilities for creating RAT virus")
-print("  # \033[1;34m[ 07 ] >> \033[1;36;40mSQL injection-Tool:  -  Tools for creating viruses")
-print("  # \033[1;34m[ 08 ] >> \033[1;36;40mSocialMedia Hacking:  -  Tools for hacking social networks")
-print("  # \033[1;34m[ 09 ] >> \033[1;36;40mSMS-spaming Tools:  -  Utilities for spam and anonymous SMS")
-print("  # \033[1;34m[ 10 ] >> \033[1;36;40mVulnerability Analysis:  -  Systems for vulnerability analysis")
-print("  # \033[1;34m[ 11 ] >> \033[1;36;40mDarkSearch Tool:  -  Tools for Secret and Onion search ")
-print("  # \033[1;34m[ 12 ] >> \033[1;36;40mPhishing Tool:  -  Tools for phishing and fake websites")
-print("  # \033[1;34m[ 13 ] >> \033[1;36;40mPassworld Attack:  -  Utilities for cracking passwords")
-print("")
-print("  # \033[1;34m[ 14 ] >> \033[1;36;40mOther tools:  -  Separate and other tools for hacking")
-print("  # \033[1;34m[ 15 ] >> \033[1;36;40mTermux Panel:  -  Termux control and special features")
-print("  # \033[1;34m[ 16 ] >> \033[1;36;40mUpdate System:  -  Update system and all tools & menu")
-print("  # \033[1;34m[ 17 ] >> \033[1;36;40mAbout System:  -  View system information and developer")
-print("  # \033[1;34m[ 18 ] >> \033[1;36;40mExit System:  -  log out system")
 
 op=int(raw_input("Options: "))
 
