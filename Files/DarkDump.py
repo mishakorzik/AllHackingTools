763c26b833e16acee27678d080e948c3c65c01b2
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-13 12:31:51 +0300

    Create DarkDump.py

---
+++ Files/DarkDump.py
@@ -0,0 +1,14 @@
+import requests
+import os
+import time
+import random
+import sys
+os.system("clear")
+os.system("cd")
+os.system("cd AllHackingTools")
+os.system("bash Logo.sh")
+os.system("cd darkdump")
+site=int(raw_input("\033[1;33;40mEnter you url Site :"))
+os.system("python3 darkdump.py '+site")
+os.system("cd")
+os.system("cd AllHackingTools")
