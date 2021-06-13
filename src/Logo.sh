68e0623c0cd8b29086b9344bed49a940128cba86
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-13 23:09:07 +0300

    Update Logo.sh

--- src/Logo.sh
+++ src/Logo.sh
@@ -9,4 +9,4 @@ echo '
 [+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
 [+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
 [+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
-[+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'|lolcat -a -d 8 -s 35.0 -p 1.6
+[+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'|lolcat -a -d 7 -s 35.0 -p 1.6
