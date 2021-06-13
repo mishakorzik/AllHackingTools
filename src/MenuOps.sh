26e491832695f034ccdaef48418f5302498a5562
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-13 11:02:57 +0300

    Update MenuOps.sh

--- src/MenuOps.sh
+++ src/MenuOps.sh
@@ -5,6 +5,7 @@ echo '
   # [ 04 ] >> Web Hacking Tool:  -  Tools for hacking websites and servers
   # [ 05 ] >> Cam Hacking Tool:  -  Tools for hacking cams and front camera
   # [ 06 ] >> InjectionRAT Utilites:  -  Utilities for creating RAT virus
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
   # [ 07 ] >> SQL injection-Tool:  -  Tools for creating viruses
   # [ 08 ] >> SocialMedia Hacking:  -  Tools for hacking social networks
   # [ 09 ] >> SMS-spaming Tools:  -  Utilities for spam and anonymous SMS
@@ -12,7 +13,7 @@ echo '
   # [ 11 ] >> DarkSearch Tool:  -  Tools for Secret and Onion search 
   # [ 12 ] >> Phishing Tool:  -  Tools for phishing and fake websites
   # [ 13 ] >> Passworld Attack:  -  Utilities for cracking passwords
-  [------------------------------Other-----------------------------]
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣ 
   # [ 14 ] >> Other tools:  -  Separate and other tools for hacking
   # [ 15 ] >> Termux Panel:  -  Termux control and special features
   # [ 16 ] >> Update System:  -  Update system and all tools & menu
