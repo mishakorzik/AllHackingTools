b66a642fc7501aa8ed2e570224db55810617e91d
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-13 13:26:57 +0300

    Update MenuA.sh

--- src/MenuA.sh
+++ src/MenuA.sh
@@ -1,6 +1,5 @@
 echo ' 
  # [ 01 ] >> Back To MainMenu  -  Back to MainMenu 
  # [ 02 ] >> Reboot system  -  Reboot All tools
- # [ 03 ] >> Exit System  -  log out system
-'|lolcat -p 1.0 -a -d 10 -s 30.0
+ # [ 03 ] >> Exit System  -  log out system'|lolcat -p 1.0 -a -d 10 -s 30.0
 
