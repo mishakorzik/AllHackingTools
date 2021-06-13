5d9b674ff995f516866ace019e2d36cc467c2c17
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-13 22:35:43 +0300

    Update TermuxSFiles.sh

--- Files/TermuxSFiles.sh
+++ Files/TermuxSFiles.sh
@@ -14,3 +14,11 @@ git clone https://github.com/laraib07/TermuxBackupTools
 cd
 cd
 cd AllHackingTools
+git clone https://github.com/Bhai4You/Termux-Banner
+cd Termux-Banner/zsh
+chmod +x requirement.sh
+chmod +x t-ban.sh
+bash requirement.sh
+cd
+cd
+cd AllHackingTools
