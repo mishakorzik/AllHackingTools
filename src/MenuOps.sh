9a16aacbd8d7980bfa5396fa2bd387ac33b98f79
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-13 11:07:35 +0300

    Update MenuOps.sh

--- src/MenuOps.sh
+++ src/MenuOps.sh
@@ -4,7 +4,7 @@ echo '
   # [ 03 ] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
   # [ 04 ] >> Web Hacking Tool:  -  Tools for hacking websites and servers
   # [ 05 ] >> Cam Hacking Tool:  -  Tools for hacking cams and front camera
-  # [ 06 ] >> InjectionRAT Utilites:  -  Utilities for creating RAT virus
+  # [ 06 ] >> Injection RAT:  -  Utilities for creating RAT virus
       ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
   # [ 07 ] >> SQL injection-Tool:  -  Tools for creating viruses
   # [ 08 ] >> SocialMedia Hacking:  -  Tools for hacking social networks
@@ -18,5 +18,5 @@ echo '
   # [ 15 ] >> Termux Panel:  -  Termux control and special features
   # [ 16 ] >> Update System:  -  Update system and all tools & menu
   # [ 17 ] >> About System:  -  View system information and developer
-  # [ 18 ] >> Exit System:  -  log out system
-'|lolcat -p 2.0
+  # [ 18 ] >> Exit System:  -  log out system'|lolcat -p 2.0
+
