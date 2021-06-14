c16111349944e367b6ecc5c87a23a676bd1df4a2
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-14 12:18:58 +0300

    Update PhishingMenu.py

--- Files/PhishingMenu.py
+++ Files/PhishingMenu.py
@@ -15,8 +15,9 @@ print("  \033[1;34m[ 05 ] >> \033[1;36;40mBlackPhish - It's easy phishing tool")
 print("  \033[1;34m[ 06 ] >> \033[1;36;40mUserRecon - find people from social")
 print("  \033[1;34m[ 07 ] >> \033[1;36;40mMask Phish - An masking phishing url")
 print("  \033[1;34m[ 08 ] >> \033[1;36;40mSeeker - Accurately Locate Smartphones")
-print("  \033[1;34m[ 09 ] >> \033[1;36;40mExit Utility")
-print("  \033[1;34m[ 10 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 09 ] >> \033[1;36;40mAIOPhish - more phishing sites")
+print("  \033[1;34m[ 10 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 11 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Phishing: "))
 
@@ -50,10 +51,13 @@ elif(op==8):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd seeker && python3 seeker.py")
 elif(op==9):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd AIOPhish && bash aiophish.sh")
+elif(op==10):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
-elif(op==10):
+elif(op==11):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
