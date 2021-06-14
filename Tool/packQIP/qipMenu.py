6421c9f8b15bcf811118afa8284e518b519620b5
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-14 19:21:37 +0300

    Create qipMenu.py

---
+++ Tool/packQIP/qipMenu.py
@@ -0,0 +1,24 @@
+import requests
+import os
+import time
+import random
+import sys
+
+os.system("clear")
+os.system("cd && cd AllHackingTools && clear && bash src/RunLogo.sh")
+
+print("  \033[1;34m[1] \033[1;36;40mInstall module")
+print("  \033[1;34m[2] \033[1;36;40mReinstall module")
+print("  \033[1;34m[3] \033[1;36;40mRemove module")
+
+op=int(raw_input("qip: "))
+
+if(op==1):
+ os.system("cd && cd /data/data/com.termux/files/usr/bin/packQIP/ && bash InstallModuleqip62491.sh")
+elif(op==2):
+ os.system("cd && cd /data/data/com.termux/files/usr/bin/packQIP/ && bash ReinstallModuleqip62491.sh")
+elif(op==3):
+ os.system("cd && cd /data/data/com.termux/files/usr/bin/packQIP/ && bash RemoveModuleqip62491.sh")
+else:
+ time.sleep(0.2)
+ os.system("echo qip stopped!")
