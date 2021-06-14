710e0469fdb855c3450264a468b51cc2793d18d7
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-14 14:48:00 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,7 +3,12 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-sleep 1
+apt install python 
+apt install python2 
+apt install pip
+apt install pip2
+pip install requests
+pip2 install requests
 cd
 cd
 cd AllHackingTools
