a4ec0acccb4d64604dc987c6304610a1ca0ecc81
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-15 10:44:44 +0300

    Update SubDom.sh

--- Files/SubDom.sh
+++ Files/SubDom.sh
@@ -8,9 +8,12 @@ cd
 cd AllHackingTools
 bash src/RunLogo.sh
 cd takeover
+echo ""
 echo -e $b">>>"$w" Write the site address: "$g"TakeOver"$w
 read siteURL
-sleep 0.2
+sleep 0.1
+echo -e $b">>>"$w" Please wait a moment: "$g"TakeOver"$w
+sleep 0.6
 python3 takeover.py -d '$siteURL' -v
 cd
 cd AllHackingTools
