888d28d62eb05c5e7fe6073a439d7c26e6b058c7
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-16 23:54:48 +0300

    Create CheckPackages.py

---
+++ src/CheckPackages.py
@@ -0,0 +1,21 @@
+R = '\033[31m'
+G = '\033[32m' 
+C = '\033[36m' 
+W = '\033[0m'  
+
+from shutil import which
+
+print(G + '[ ✔ ]' + C + ' Checking Packages...' + W)
+pkgs = ['python', 'pip', 'python2', 'ssh', 'lolcat', 'figlet', 'pip2', 'git', 'wget', 'openssh', 'clang', 'neofetch']
+inst = True
+for pkg in pkgs:
+        present = which(pkg)
+        if present == None:
+                print(R + '[ ✘ ] ' + W + packages + C + ' is not Installed!')
+                inst = False
+        else:
+                pass
+if inst == False:
+        exit()
+else:
+        pass
