6c1d4e1db01b6e3b4cb490faebd78905fdcf1c80
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-17 00:31:50 +0300

    Update CheckPackages.py

--- src/CheckPackages.py
+++ src/CheckPackages.py
@@ -4,8 +4,10 @@ C = '\033[36m'
 W = '\033[0m'  
 
 from shutil import which
+import time
 
 print(G + '[ ✔ ]' + C + ' Checking Packages...' + W)
+time.sleep(1)
 pkgs = ['python', 'pip', 'python2', 'ssh', 'lolcat', 'figlet', 'pip2', 'git', 'wget', 'clang', 'neofetch', 'pv', 'zsh', 'curl', 'w3m']
 inst = True
 for pkg in pkgs:
@@ -20,4 +22,6 @@ if inst == False:
 else:
         pass
 print(G + '[ ✔ ]' + C + ' Succesfull checked! modules...' + W)
+time.sleep(0.2)
 print(G + '[ ✔ ]' + C + ' All modules installed...' + W)
+time.sleep(0.1)
