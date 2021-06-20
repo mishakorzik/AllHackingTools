80baa2dea3dbb21a66c1a1b6ff3ba46385eb2d18
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:41:11 +0300

    Update AnalistickMenu.py

--- Files/AnalistickMenu.py
+++ Files/AnalistickMenu.py
@@ -15,35 +15,35 @@ print("  \033[1;34m[ 05 ] >> \033[1;36;40mRang3r - Multi Thread IP + Port Scanne
 print("  \033[1;34m[ 06 ] >> \033[1;36;40mSqlmap - Automatic SQL injection and database takeover tool")
 print("  \033[1;34m[ 07 ] >> \033[1;36;40mMaxSubdoFinder - Tool for Discovering Subdomain")
 print("  \033[1;34m[ 08 ] >> \033[1;36;40mEasymap - Nmap Shortcut")
-print("  \033[1;34m[ 09 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 09 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 10 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("1nf0rmatI0n: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd AstraNmap && bash astranmap.sh")
+ os.system("cd && cd AllHackingTools && cd AstraNmap && bash astranmap.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==8):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Easymap && php easymap.php")
+ os.system("cd && cd AllHackingTools && cd Easymap && php easymap.php && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd weeman && python2 weeman.py")
+ os.system("cd && cd AllHackingTools && cd weeman && python2 weeman.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd RED_HAWK && php rhawk.php")
+ os.system("cd && cd AllHackingTools && cd RED_HAWK && php rhawk.php && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd TM-scanner && python2 tmscanner.py")
+ os.system("cd && cd AllHackingTools && cd TM-scanner && python2 tmscanner.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==7):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd MaxSubdoFinder && python2 maxteroit.py")
+ os.system("cd && cd AllHackingTools && cd MaxSubdoFinder && python2 maxteroit.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==6):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd sqlmap-dev && python sqlmap.py -wizard")
+ os.system("cd && cd AllHackingTools && cd sqlmap-dev && python sqlmap.py -wizard && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==5):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd rang3r && python2 rang3r.py --ip 192.168.0.1")
+ os.system("cd && cd AllHackingTools && cd rang3r && python2 rang3r.py --ip 192.168.0.1 && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==9):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
