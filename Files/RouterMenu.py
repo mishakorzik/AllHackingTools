74e0e2d8ecfedf3c7ed97af2f71bfe939537a685
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:49:14 +0300

    Update RouterMenu.py

--- Files/RouterMenu.py
+++ Files/RouterMenu.py
@@ -11,23 +11,23 @@ print("  \033[1;34m[ 01 ] >> \033[1;36;40mRouterSploit - universality instrument
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mWebSploit - An advanced MiTM Framework")
 print("  \033[1;34m[ 03 ] >> \033[1;36;40mCommix - OS Command Injection Exploitation Tool.")
 print("  \033[1;34m[ 04 ] >> \033[1;36;40mTxtool - an easy pentesting tool.")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("RouterHack: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd routersploit && python3 rsf.py")
+ os.system("cd && cd AllHackingTools && cd routersploit && python3 rsf.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd websploit && sudo websploit")
+ os.system("cd && cd AllHackingTools && cd websploit && sudo websploit && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd commix && python commix.py --wizard")
+ os.system("cd && cd AllHackingTools && cd commix && python commix.py --wizard && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd txtool && txtool")
+ os.system("cd && cd AllHackingTools && cd txtool && txtool && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
