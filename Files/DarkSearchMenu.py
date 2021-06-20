01b131538b2426170043c1e4a7d98d4049746dc7
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:43:05 +0300

    Update DarkSearchMenu.py

--- Files/DarkSearchMenu.py
+++ Files/DarkSearchMenu.py
@@ -8,14 +8,14 @@ os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mDarkDump - Search The Deep Web")
-print("  \033[1;34m[ 02 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 03 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("DarkSearch: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && python2 Files/DarkDump.py")
+ os.system("cd && cd AllHackingTools && python2 Files/DarkDump.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
