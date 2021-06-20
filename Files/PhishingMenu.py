ece816eaa2818b0786ad6f8c9e928cacae717af3
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:48:08 +0300

    Update PhishingMenu.py

--- Files/PhishingMenu.py
+++ Files/PhishingMenu.py
@@ -16,43 +16,43 @@ print("  \033[1;34m[ 06 ] >> \033[1;36;40mUserRecon - find people from social")
 print("  \033[1;34m[ 07 ] >> \033[1;36;40mMask Phish - An masking phishing url")
 print("  \033[1;34m[ 08 ] >> \033[1;36;40mSeeker - Accurately Locate Smartphones")
 print("  \033[1;34m[ 09 ] >> \033[1;36;40mAIOPhish - more phishing sites")
-print("  \033[1;34m[ 10 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 10 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 11 ] >> \033[1;36;40mBack To MainMenu")
 
-op=int(raw_input("Phishing: "))
+op=int(raw_input("Phish1ng: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd shark && shark")
+ os.system("cd && cd AllHackingTools && cd shark && shark && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd zphisher && bash zphisher.sh")
+ os.system("cd && cd AllHackingTools && cd zphisher && bash zphisher.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd ShellPhish && bash shellphish.sh")
+ os.system("cd && cd AllHackingTools && cd ShellPhish && bash shellphish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd saycheese && bash saycheese.sh")
+ os.system("cd && cd AllHackingTools && cd saycheese && bash saycheese.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==5):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd BlackPhish && sudo python3 blackphish.py")
+ os.system("cd && cd AllHackingTools && cd BlackPhish && sudo python3 blackphish.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==6):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Castom && bash userrecon.sh")
+ os.system("cd && cd AllHackingTools && bash Castom/userrecon.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==7):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Mask-Phish.Termux && bash Mask-Phish.sh")
+ os.system("cd && cd AllHackingTools && cd Mask-Phish.Termux && bash Mask-Phish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==8):
  os.system("")
  print "please open window 2 and enable ngrok with the command ./ngrok http 8080"
- time.sleep(1)
+ time.sleep(2)
  print "if you do not have ngrok then register on the website https://ngrok.com/ and read the download instructions"
  os.system("cd && cd AllHackingTools && python3 Timer2.py")
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd seeker && python3 seeker.py")
+ os.system("cd && cd AllHackingTools && cd seeker && python3 seeker.py -t manual && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==9):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd AIOPhish && bash aiophish.sh")
+ os.system("cd && cd AllHackingTools && cd AIOPhish && bash aiophish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==10):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
