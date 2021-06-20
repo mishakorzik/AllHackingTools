261e6ed2a64dd590afbca046254b536eaf923c6f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:44:21 +0300

    Update MailMenu.py

--- Files/MailMenu.py
+++ Files/MailMenu.py
@@ -9,17 +9,17 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mMailHack - Email hacker")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mPwnedOrnot - Tool for find passworld email")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Ma1lHacK: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd hack-gmail && python3 hack-gmail.py")
+ os.system("cd && cd AllHackingTools && cd hack-gmail && python3 hack-gmail.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd pwnedOrNot && python3 pwnedornot.py")
+ os.system("cd && cd AllHackingTools && cd pwnedOrNot && python3 pwnedornot.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
