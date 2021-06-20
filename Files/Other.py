910ed694afbf625fee9af51454e79a83fad84226
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:44:58 +0300

    Update Other.py

--- Files/Other.py
+++ Files/Other.py
@@ -9,17 +9,17 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mFree-Proxy  -  more free proxy servers")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mProxyGen  -  Free Proxy Generator")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit Utility  -  exit AllHackingTools")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit System  -  exit AllHackingTools")
 print("  \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu  -  Back to MainMenu")
 
 op=int(raw_input("OthErTo01s: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && bash Files/Proxy/Logo.sh && python2 Files/Proxy/menu.py")
+ os.system("cd && cd AllHackingTools && bash Files/Proxy/Logo.sh && python2 Files/Proxy/menu.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd ProxyGen && python2 proxygen.py")
+ os.system("cd && cd AllHackingTools && cd ProxyGen && python2 proxygen.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
