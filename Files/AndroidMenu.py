c3d83bd3918d95c705f21b89be12393782524e9c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 20:41:48 +0300

    Update AndroidMenu.py

--- Files/AndroidMenu.py
+++ Files/AndroidMenu.py
@@ -9,17 +9,17 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mPyshell - Remote Access Trojan - RAT")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mWishfish - Powerful Tool For Grab Front Camera Snap Using A Link")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit Utility")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Ma1lHacK: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd pyshell && ./Pyshell")
+ os.system("cd && cd AllHackingTools && cd pyshell && ./Pyshell && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd wishfish && ./wishfish.sh")
+ os.system("cd && cd AllHackingTools && cd wishfish && ./wishfish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
