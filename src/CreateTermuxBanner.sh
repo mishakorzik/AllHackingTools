fe9f26c90f693ac950b0ab3c9973862da7469050
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-20 18:41:28 +0300

    Update CreateTermuxBanner.sh

--- src/CreateTermuxBanner.sh
+++ src/CreateTermuxBanner.sh
@@ -1,3 +1,14 @@
+cd
+cd 
+cd /data/data/com.termux/files/usr/etc/
+rm -rf zshrc
+cd
+cd
+cd AllHackingTools
+cd Tool
+cp zshrc /data/data/com.termux/files/usr/etc/
+cd
+cd
 clear
 figlet -f Termux Banner | lolcat
 
