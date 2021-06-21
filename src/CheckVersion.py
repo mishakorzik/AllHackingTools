820bbb3992cc4ef18002103ae51ccdb7cade173a
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-21 22:20:09 +0300

    Update CheckVersion.py

--- src/CheckVersion.py
+++ src/CheckVersion.py
@@ -7,7 +7,7 @@ W = '\033[0m'  # white
 from shutil import which
 
 print(G + '[+]' + C + ' Checking Dependencies...' + W)
-pkgs = ['python3', 'pip', 'php', 'ssh', 'openssh', 'ssh', 'pip2', 'wget', 'curl', 'python', 'python2', 'toilet', 'neofetch', 'figlet', 'lolcat']
+pkgs = ['python3', 'pip', 'php', 'openssh', 'ssh', 'pip2', 'wget', 'curl', 'python', 'python2', 'toilet', 'neofetch', 'figlet', 'lolcat', 'clang', 'w3m', 'jq', 'ruby', 'ssl', 'pv']
 inst = True
 for pkg in pkgs:
 	present = which(pkg)
