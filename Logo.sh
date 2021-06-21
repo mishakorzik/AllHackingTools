86f3c2b88b49111151fd2cd26405c780e2b39417
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-21 20:25:47 +0300

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -10,4 +10,4 @@ echo '
 [+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
 [+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
 [+]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[+]
-'|lolcat -p 1.6
+'|lolcat -p 1.6 -a -d 2 -s 20.0
