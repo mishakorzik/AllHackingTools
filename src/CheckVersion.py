cf559e4cb09b592f1885e15843aee221de2898f0
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-21 23:12:28 +0300

    Update CheckVersion.py

--- src/CheckVersion.py
+++ src/CheckVersion.py
@@ -34,7 +34,6 @@ row = []
 info = ''
 result = ''
 version = '1.6'
-systemAHT = '1.2.4'
 
 def ver_check():
 	print(G + '[+]' + C + ' Checking for Updates....', end='')
@@ -46,7 +45,7 @@ def ver_check():
 			github_ver = ver_rqst.text
 			github_ver = github_ver.strip()
 
-			elif version == github_ver:
+			if version == github_ver:
 				print(C + '[' + G + ' No Updates ' + C +']' + '\n')
 			else:
 				print(C + '[' + R + ' Available : {} '.format(github_ver) + C + ']' + '\n')
@@ -54,28 +53,8 @@ def ver_check():
 			print(C + '[' + R + ' Status : {} '.format(ver_sc) + C + ']' + '\n')
 	except Exception as e:
 		print('\n' + R + '[-]' + C + ' Exception : ' + W + str(e))
-
-def set_check():
-	print(G + '[+]' + C + ' Checking Tools Setting....', end='')
-	sys_url = 'https://raw.githubusercontent.com/mishakorzik/AllHackingTools/main/Castom/system.txt'
-	try:
-		sys_rqst = requests.get(sys_url)
-		sys_sc = sys_rqst.status_code
-		if sys_sc == 200:
-			github_sys = sys_rqst.text
-			github_sys = github_sys.strip()
-
-			if systemAHT == github_sys:
-				print(C + '[' + G + ' No System Updates ' + C +']' + '\n')
-			else:
-				print(C + '[' + R + ' The System Is Outdated : {} '.format(github_sys) + C + ']' + '\n')
-		else:
-			print(C + '[' + R + ' Status : {} '.format(sys_sc) + C + ']' + '\n')
-	except Exception as e:
-		print('\n' + R + '[-]' + C + ' Exception : ' + W + str(e))
 try:
 	ver_check()
-        set_check()
 
 except KeyboardInterrupt:
 	print ('\n' + R + '[!]' + C + ' Keyboard Interrupt.' + W)
