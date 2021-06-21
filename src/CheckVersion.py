b79a88a2bdc9ffbc6b8bda214386d3882811e300
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-21 21:21:31 +0300

    Create CheckVersion.py

---
+++ src/CheckVersion.py
@@ -0,0 +1,22 @@
+def check_version():
+	print(G + '[+]' + C + ' Checking for Updates....', end='')
+	ver_url = 'https://raw.githubusercontent.com/mishakorzik/AllHackingTools/main/Castom/version.txt'
+	try:
+		ver_rqst = requests.get(ver_url)
+		ver_sc = ver_rqst.status_code
+		if ver_sc == 200:
+			github_ver = ver_rqst.text
+			github_ver = github_ver.strip()
+
+			if version == github_ver:
+				print(C + '[' + G + ' Up-To-Date ' + C +']' + '\n')
+			else:
+				print(C + '[' + G + ' Available : {} '.format(github_ver) + C + ']' + '\n')
+		else:
+			print(C + '[' + R + ' Status : {} '.format(ver_sc) + C + ']' + '\n')
+	except Exception as e:
+		print('\n' + R + '[-]' + C + ' Exception : ' + W + str(e))
+
+try:
+	check_version()
+	
