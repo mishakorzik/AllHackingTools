bd085e123609f5bb277c442b64187702a284f780
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-21 20:12:36 +0300

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -173,9 +173,6 @@ bash TermuxNewKeys.sh
 cd
 cd
 clear
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-cd
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 cd
 cd
 rm -rf Termux-os
