5cd9a21627ce0099fad2516ca82f9e1d35e138c5
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-22 10:18:49 +0300

    Update Updater.sh

--- Files/Updater.sh
+++ Files/Updater.sh
@@ -9,11 +9,11 @@ rm -rf AutoUpdateMyTools
 cd 
 git clone https://github.com/mishakorzik/AutoUpdateMyTools 
 cd AutoUpdateMyTools
-echo $w"["$o"WARN"$w"]"$b"verifining settings!"$w
+echo -e $w"["$o"WARN"$w"]"$b"verifining settings!"$w
 sleep 0.2
-echo $w"["$o"WARN"$w"]"$b"I don't close termux app!"$w
+echo -e $w"["$o"WARN"$w"]"$b"I don't close termux app!"$w
 sleep 1
-echo $w"["$g"INFO"$w"]"$b"Succesfull verifined!"$w
+echo -e $w"["$g"INFO"$w"]"$b"Succesfull verifined!"$w
 sleep 0.9
 cd
 cd
