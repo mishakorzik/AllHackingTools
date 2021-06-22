4cc2375d79e545f58306432355799a62c7681d46
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-22 23:08:00 +0300

    Update SocialMenu.py

--- Files/SocialMenu.py
+++ Files/SocialMenu.py
@@ -7,7 +7,7 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  \033[1;34m[ 01 ] >> \033[1;36;40mERROR404 - NOT FOUND")
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mInstaHack - Best Tool For instagram bruteforce hacking Tool")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 03 ] >> \033[1;36;40mBack To MainMenu")
 
@@ -15,7 +15,7 @@ op=int(raw_input("Soc1alF1sh: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd AllHackingTools && cd instahack && bash instahack.sh")
 elif(op==2):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
