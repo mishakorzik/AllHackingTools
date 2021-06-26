ea6ebfb1581d0a90494391badcda031ac5cb4007
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-26 22:17:22 +0300

    Create banner0.sh

---
+++ .banners/banner0.sh
@@ -0,0 +1,5 @@
+ [+]  //     //  \\     Developer: Misha Korzhik
+ [+] //     //    \\ [-]—————————Other————————[-]
+  |  \\    //     //  | Logo created on: 2021-26-06
+ [+]  \\  //     //   | Company: FlyZero copyring
+ [-]———–———-–—————–-—[+]————————————————-–————[+]
