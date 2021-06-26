4928d27fc84d3aa977b8451d14481c64921b3c06
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-26 22:21:37 +0300

    Update banner0.sh

--- .banners/banner0.sh
+++ .banners/banner0.sh
@@ -1,5 +1,7 @@
+echo '
  [+]  //     //  \\     Developer: Misha Korzhik
  [+] //     //    \\ [-]—————————Other————————[-]
   |  \\    //     //  | Logo created on: 2021-26-06
  [+]  \\  //     //   | Company: FlyZero copyring
  [-]———–———-–—————–-—[+]————————————————-–————[+]
+'| lolcat -p 1.0
