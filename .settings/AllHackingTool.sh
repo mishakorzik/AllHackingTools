b130ce21cdd5943b4264ec1b0a45e94bbad110ac
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-27 23:12:19 +0300

    Create AllHackingTool.sh

---
+++ .settings/AllHackingTool.sh
@@ -0,0 +1,25 @@
+cd
+cd
+cd AllHackingTools
+python3 src/CheckVersion.py
+sleep 3
+clear
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+echo '
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╱╱╱╱╱╭━━━━╮╱╱╭━━━╮╭╮╱╱╱╱╱╭╮
+┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱┃╭━━╯╱╭╯╰╮╱╱╱╱╱┃╭╮╭╮┃╱╱┃╭━╮┣╯╰╮╱╱╱╭╯╰╮
+┃╰━╯┣━┳━━┳━━┳━━╮┃╰━━┳━╋╮╭╋━━┳━╮╰╯┃┃┣┻━╮┃╰━━╋╮╭╋━━┳┻╮╭╯
+┃╭━━┫╭┫┃━┫━━┫━━┫┃╭━━┫╭╮┫┃┃┃━┫╭╯╱╱┃┃┃╭╮┃╰━━╮┃┃┃┃╭╮┃╭┫┃
+┃┃╱╱┃┃┃┃━╋━━┣━━┃┃╰━━┫┃┃┃╰┫┃━┫┃╱╱╱┃┃┃╰╯┃┃╰━╯┃┃╰┫╭╮┃┃┃╰╮
+╰╯╱╱╰╯╰━━┻━━┻━━╯╰━━━┻╯╰┻━┻━━┻╯╱╱╱╰╯╰━━╯╰━━━╯╰━┻╯╰┻╯╰━╯
+Please press enter to launch...'|lolcat -p 1.7 -a -d 7 -s 45.0
+read a1
+cd
+cd
+cd AllHackingTools 
+mv ErrorServer254NotFound.py MainMenu.py
+echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
+python2 MainMenu.py
