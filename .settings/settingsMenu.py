3a5b516843506d4637dbac1d41a055804d9eeeff
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-28 00:01:52 +0300

    Update settingsMenu.py

--- .settings/settingsMenu.py
+++ .settings/settingsMenu.py
@@ -27,7 +27,7 @@ elif(op==3):
  os.system("cd && cd AllHackingTools && cd src && mv AnimationLoad1.sh /data/data/com.termux/files/home/AllHackingTools/.temp/ && cd && cd AllHackingTools && cd .settings && mv AnimationLoad1.sh /data/data/com.termux/files/home/AllHackingTools/src/ && cd && cd AllHackingTools && cd .temp && mv AnimationLoad1.sh /data/data/com.termux/files/home/AllHackingTools/.settings/")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd .settings && python2 SpecialOpportunities.py
+ os.system("cd && cd AllHackingTools && cd .settings && python2 SpecialOpportunities.py")
 elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
