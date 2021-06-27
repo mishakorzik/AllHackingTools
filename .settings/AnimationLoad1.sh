9927379f3f4789c3409f6fe3ce92ffa001a0c4ca
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-27 22:46:49 +0300

    Create AnimationLoad1.sh

---
+++ .settings/AnimationLoad1.sh
@@ -0,0 +1,119 @@
+clear
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+printf "\e[1;92m"
+
+printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+termux-vibrate -d 150 -f
+printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.01
+clear
+printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
+sleep 0.04
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ..\n"
+sleep 0.05
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
+sleep 0.005
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+termux-vibrate -d 150 -f
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+help
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading .\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ..\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
+leep 0.02
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Succesfull!\n"
+sleep 0.2
+cd
+cd
+cd AllHackingTools
+termux-vibrate -d 100 -f
+python3 .check/Configuration.py
