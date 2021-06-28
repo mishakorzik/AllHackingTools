3b04640cb2d5471aedfbfb2e3e739d3cd8421b8c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-28 10:00:35 +0300

    Update settingsMenu.py

--- .settings/settingsMenu.py
+++ .settings/settingsMenu.py
@@ -7,9 +7,9 @@ import sys
 os.system("clear")
 os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
-print("  \033[1;34m[ 01 ] >> \033[1;36;40mLogo Setting")
-print("  \033[1;34m[ 02 ] >> \033[1;36;40mMenu Setting")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mVibration Setting")
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mLogo Setting - d/n")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mMenu Setting - d/n")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mVibration Setting - on/off")
 print("  \033[1;34m[ 04 ] >> \033[1;36;40mSpecial Opportunities")
 print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System")
 print("  \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
