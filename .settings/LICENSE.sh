c04fe6d8999c13778773f21c1576dc1a6d7996ec
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-28 10:08:15 +0300

    Update LICENSE.sh

--- .settings/LICENSE.sh
+++ .settings/LICENSE.sh
@@ -1,4 +1,5 @@
-GNU GENERAL PUBLIC LICENSE
+echo """
+                      GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
 
  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
@@ -671,5 +672,5 @@ into proprietary programs.  If your program is a subroutine library, you
 may consider it more useful to permit linking proprietary applications with
 the library.  If this is what you want to do, use the GNU Lesser General
 Public License instead of this License.  But first, please read
-
-  
+<https://www.gnu.org/licenses/why-not-lgpl.html>.
+"""| lolcat -p 50.0
