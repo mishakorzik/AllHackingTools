e26b81511f55012be266ee243c12c7ea2a460f9b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-28 10:22:22 +0300

    Create Applined.sh

---
+++ .settings/Applined.sh
@@ -0,0 +1,9 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+echo -e $w"["$g"INFO"$w"]"$b"SuccesFull Applined Settings!"$w
+sleep 0.1
+echo -e $w"["$o"WARN"$w"]"$b"Please Start AllHackingTools To View!"$w
