4f8e14ce325f580ec553fa92d033a29fe269bc23
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-29 22:31:47 +0300

    Create MenuOps2.sh

---
+++ src/MenuOps2.sh
@@ -0,0 +1,24 @@
+echo '
+  [01] >> Information Gathering:  -  Collects information from database
+  [02] >> Exploitation Tools:  -  selection of tools for operation and hacking
+  [03] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
+  [04] >> Web Hacking Tool:  -  Tools for hacking websites and servers
+  [05] >> Cam Hacking Tool:  -  Tools for hacking cams and front camera
+  [06] >> Injection RAT:  -  Utilities for creating RAT virus
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
+  [07] >> SQL injection-Tool:  -  Tools for creating viruses
+  [08] >> SocialMedia Hacking:  -  Tools for hacking social networks
+  [09] >> SMS-spaming Tools:  -  Utilities for spam and anonymous SMS
+  [10] >> Vulnerability Analysis:  -  Systems for vulnerability analysis
+  [11] >> DarkSearch Tool:  -  Tools for Secret and Onion search 
+  [12] >> Phishing And IpHack:  -  Tools for phishing and fake websites
+  [13] >> Passworld Attack:  -  Utilities for cracking passwords
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣ 
+  [14] >> Other tools:  -  Separate and other tools for hacking
+  [15] >> Termux Panel:  -  Termux control and special features
+  [16] >> System Settings:  -  AllHackingTools control and special features
+  [17] >> System License:  -  View A AllHackingTools License =  License
+  [18] >> Update System:  -  Update system and all tools & menu and more utilitys
+  [19] >> About System:  -  View system information and developer
+  [20] >> Exit System:  -  Log out of the AllHackingTools system '|lolcat -p 2.0
+
