9f29dd69eb6df28d052fc74c4d5ef8e7935bdd0b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-29 15:02:10 +0300

    Update IpMenu.py

--- Files/IpMenu.py
+++ Files/IpMenu.py
@@ -18,8 +18,9 @@ print("  \033[1;34m[ 08 ] >> \033[1;36;40mRed Hawk - Information Gathering, Vuln
 print("  \033[1;34m[ 09 ] >> \033[1;36;40mLittleBrother - Information gathering (OSINT) on a person (EU)")
 print("  \033[1;34m[ 10 ] >> \033[1;36;40mSeeker - Accurately Locate Smartphones and GPS")
 print("  \033[1;34m[ 11 ] >> \033[1;36;40mReconDog - Reconnaissance Swiss Army Knife")
-print("  \033[1;34m[ 12 ] >> \033[1;36;40mExit System")
-print("  \033[1;34m[ 13 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 12 ] >> \033[1;36;40mD-Tech - Pentesting the Modern Web")
+print("  \033[1;34m[ 13 ] >> \033[1;36;40mExit System")
+print("  \033[1;34m[ 14 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("1nf0rmatI0n: "))
 
@@ -62,10 +63,13 @@ elif(op==11):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd ReconDog && ./dog && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==12):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd D-Tech && python2 d-tect.py && cd && cd AllHackingTools && python2 MainMenu.py")
+elif(op==13):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==13):
+elif(op==14):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
