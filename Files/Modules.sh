a49ccd6d04ff365997381e3921cdea9fbc968d9a
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-06 10:46:44 +0300

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -138,6 +138,9 @@ cp msdc /data/data/com.termux/files/usr/bin/
 cp msdconsole /data/data/com.termux/files/usr/bin/
 cp msdconsoleUPD /data/data/com.termux/files/usr/bin/
 cp msdServer /data/data/com.termux/files/usr/bin/
+cp msd /data/data/com.termux/files/usr/bin/
+cp ms /data/data/com.termux/files/usr/bin/
+cp m /data/data/com.termux/files/usr/bin/
 cd
 cd
 cd /data/data/com.termux/files/usr/bin/
@@ -145,6 +148,9 @@ chmod +x msdconsole
 chmod +x msdconsoleUPD
 chmod +x msdc
 chmod +x msdServer
+chmod +x msd
+chmod +x ms
+chmod +x m
 ls
 sleep 0.2
 clear
