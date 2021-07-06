ca34d6175fba06091b7a152377f4a3d3989429f1
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-06 19:16:16 +0300

    Create XSScon.sh

---
+++ Files/XSScon.sh
@@ -0,0 +1,20 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+clear
+cd
+cd AllHackingTools
+bash Logo.sh
+cd XSSCon
+echo ""
+echo -e $b">>>"$w" Write the site address: "$g"TakeOver"$w
+read siteURL1
+sleep 0.1
+echo -e $b">>>"$w" Please wait a moment: "$g"TakeOver"$w
+sleep 0.6
+python3 xsscon.py -u $siteURL1 -v
+cd
+cd
+cd AllHackingTools
