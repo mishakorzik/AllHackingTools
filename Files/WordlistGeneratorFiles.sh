6cfaaa4c0e4351a07d8c08c8d276a9079e764a2c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-06 15:24:50 +0300

    Create WordlistGeneratorFiles.sh

---
+++ Files/WordlistGeneratorFiles.sh
@@ -0,0 +1,20 @@
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/Keshav2136/wlcreator 
+cd wlcreator 
+gcc -o wlcreator wlcreator.c 
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/UndeadSec/GoblinWordGenerator.git
+cd GoblinWordGenerator
+cd
+cd 
+cd AllHackingTools
+git clone https://github.com/Viralmaniar/SMWYG-Show-Me-What-You-Got.git
+cd SMWYG-Show-Me-What-You-Got
+pip2 install -r requirements.txt
+cd
+cd
+cd AllHackingTools
