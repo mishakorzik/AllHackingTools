e1c171bd93b6ef22fe8203652b7b0e3ec7ade6f1
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-07 19:23:40 +0300

    Create StartAllHackingToolsAndTerminal.sh

---
+++ .settings/StartAllHackingToolsAndTerminal.sh
@@ -0,0 +1,16 @@
+cd
+cd 
+cd /data/data/com.termux/files/usr/etc/
+rm -rf zshrc
+cd
+cd
+cd AllHackingTools
+cd Tool
+cp zshrc /data/data/com.termux/files/usr/etc/
+cd
+cd
+echo "cd && cd AllHackingTools && python2 MainMenu.py" > sTa4t.txt
+cat "sTa4t.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cd
+cd
+
