8ab703639080c74b1de21346f3232e201da05304
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-07 19:40:44 +0300

    Update StartAllHackingToolsAndTerminal.sh

--- .settings/StartAllHackingToolsAndTerminal.sh
+++ .settings/StartAllHackingToolsAndTerminal.sh
@@ -11,6 +11,8 @@ cd
 cd
 echo "cd && cd AllHackingTools && python2 MainMenu.py" > sTa4t.txt
 cat "sTa4t.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+rm -rf sTa4t.txt
 cd
 cd
+rm -rf sTa4t.txt
 
