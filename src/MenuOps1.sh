3f7d1fb4b3f7ce79e1c4cf1338a4698593eed1f7
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-08 10:10:12 +0300

    Update MenuOps1.sh

--- src/MenuOps1.sh
+++ src/MenuOps1.sh
@@ -3,13 +3,13 @@ echo '
   [ 01 ] >> Information Gathering:  -  Collects information from database
   [ 02 ] >> Exploitation Tools:  -  selection of tools for operation and hacking
   [ 03 ] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
-  [ 04 ] >> Web Hacking Tool:  -  Tools for hacking websites and servers
-  [ 05 ] >> Cam Hacking Tool:  -  Tools for hacking cams and front camera
-  [ 06 ] >> Injection RAT:  -  Utilities for creating RAT virus
+  [ 04 ] >> Web Attack Tools:  -  Tools for hacking websites and servers
+  [ 05 ] >> Cam Hacking Tools:  -  Tools for hacking cams and front camera
+  [ 06 ] >> Remote Trojan RAT:  -  Utilities for creating RAT virus
     [NOTICE] Useful Tools  ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣ 
   [ 07 ] >> SQL injection-Tool:  -  Tools for creating viruses
   [ 08 ] >> SocialMedia Hacking:  -  Tools for hacking social networks
-  [ 09 ] >> SMS-spaming Tools:  -  Utilities for spam and anonymous SMS
+  [ 09 ] >> SMS spaming Tools:  -  Utilities for spam and anonymous SMS
   [ 10 ] >> Vulnerability Analysis:  -  Systems for vulnerability analysis
   [ 11 ] >> DarkSearch Tool:  -  Tools for Secret and Onion search 
   [ 12 ] >> Phishing And IpHack:  -  Tools for phishing and fake websites
@@ -20,8 +20,7 @@ echo '
   [ 16 ] >> Other tools:  -  Separate and other tools for hacking
   [ 17 ] >> Termux Panel:  -  Termux control and special features
   [ 18 ] >> System Settings:  -  AllHackingTools control and special features
-  [ 19 ] >> System License:  -  View A AllHackingTools License =  License
-  [ 20 ] >> Update System:  -  Update system and all tools & menu
+  [ 19 ] >> System License:  -  View the license in AllHackingTools
+  [ 20 ] >> Update System:  -  Update system and all tools & menu and more utilitys
   [ 21 ] >> About System:  -  View system information and developer
   [ 22 ] >> Exit System:  -  Log out of the AllHackingTools system '|lolcat -p 1.2
-
