cc5e3a3e1bdf666d442ae7497348b5aeaec34d23
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-09 17:40:29 +0300

    Update settingsMenu.py

--- .settings/settingsMenu.py
+++ .settings/settingsMenu.py
@@ -11,8 +11,10 @@ print("  \033[1;34m[ 01 ] >> \033[1;36;40mLogo Setting - d/n")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mMenu Setting - d/n")
 print("  \033[1;34m[ 03 ] >> \033[1;36;40mVibration Setting - on/off")
 print("  \033[1;34m[ 04 ] >> \033[1;36;40mSpecial Opportunities")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System")
-print("  \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mView My Activity")
+print("  \033[1;34m[ 06 ] >> \033[1;36;40mDelete My Activity")
+print("  \033[1;34m[ 07 ] >> \033[1;36;40mExit System")
+print("  \033[1;34m[ 08 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Sett1Ngs: "))
 
@@ -29,10 +31,16 @@ elif(op==4):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd .settings && python2 SpecialOpportunities.py")
 elif(op==5):
+ os.system("clear")
+ os.system("cd && nano .zsh_history")
+elif(op==6):
+ os.system("clear")
+ os.system("cd && rm -rf AllHackingTools/.settings/deletedfiles/.zsh_history && cp .zsh_history AllHackingTools/.settings/deletedfiles/")
+elif(op==7):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==6):
+elif(op==8):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
