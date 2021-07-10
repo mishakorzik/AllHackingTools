1bec409f5f4ba9d2b218d3cef6a84e48bbf9d1a9
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-10 23:15:18 +0300

    Create SettingMenu0.sh

---
+++ .desing/desing/SettingMenu0.sh
@@ -0,0 +1,8 @@
+echo '
+[ 01 ] >> Quick Start - on/off
+[ 02 ] >> Servers Setting
+[ 03 ] >> New ExtraKeys0
+[ 04 ] >> New ExtraKeys1
+[ 05 ] >> Add AllHackingTool to startup
+[ 06 ] >> Exit System - log out AllHackingTool
+[ 07 ] >> Back To MainMenu - go to the home menu '| lolcat -p 1.7
