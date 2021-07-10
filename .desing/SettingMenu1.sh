1313906b25218b4120ccddea513e5665d9403324
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-10 23:09:33 +0300

    Update SettingMenu1.sh

--- .desing/SettingMenu1.sh
+++ .desing/SettingMenu1.sh
@@ -1,9 +1,9 @@
 echo '
-[01] > Logo Setting - d/n
-[02] > Menu Setting - d/n
-[03] > Vibration Setting - on/off
-[04] > Special Opportunities
-[05] > View My Activity
-[06] > Delete My Activity
-[07] > Exit System
-[08] > Back To MainMenu '| lolcat -p 1.5
+[01] >> Logo Setting - d/n
+[02] >> Menu Setting - d/n
+[03] >> Vibration Setting - on/off
+[04] >> Special Opportunities
+[05] >> View My Activity
+[06] >> Delete My Activity
+[07] >> Exit System
+[08] >> Back To MainMenu '| lolcat -p 1.5
