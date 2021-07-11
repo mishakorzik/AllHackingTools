bd85a74fb6c2bc4c7ce7dea10ade8f2f9c8a56fe
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-11 10:02:17 +0300

    Update AnimationLoad1.sh

--- .settings/AnimationLoad1.sh
+++ .settings/AnimationLoad1.sh
@@ -6,111 +6,108 @@ w="\033[0m"
 o="\033[1;33m"
 printf "\e[1;92m"
 
-printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-termux-vibrate -d 150 -f
-printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
 sleep 0.01
 clear
-printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
 sleep 0.05
 clear
-printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+clear
+printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+clear
+printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
 sleep 0.01
 clear
-printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
 sleep 0.01
+termux-vibrate -d 200 -f
 clear
-printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.02
+clear
+printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+sleep 0.02
+clear
+printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
+printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+termux-vibrate -d 100 -f
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.03
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
+sleep 0.01
+clear
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
 sleep 0.05
 clear
-printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.05
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
 clear
-printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
-sleep 0.04
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ..\n"
-sleep 0.05
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
 sleep 0.005
+termux-vibrate -d 100 -f
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
 sleep 0.01
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
 sleep 0.01
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading .\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-termux-vibrate -d 150 -f
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ..\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
 help
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading .\n"
-sleep 0.01
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
+sleep 0.006
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ..\n"
-sleep 0.01
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
+sleep 0.006
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
-leep 0.02
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
+leep 0.1
 clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Succesfull!\n"
+printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  Succesfull!\n"
 sleep 0.2
 cd
 cd
