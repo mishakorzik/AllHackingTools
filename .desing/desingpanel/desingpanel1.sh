faf5b12ed1d5251f874d9191b78475d8ea644ece
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-12 23:38:33 +0300

    Update desingpanel1.sh

--- .desing/desingpanel/desingpanel1.sh
+++ .desing/desingpanel/desingpanel1.sh
@@ -1,16 +1,16 @@
 echo '
-  [01] >> View MySystem
-  [02] >> Google Search
-  [03] >> Battarey Status
-  [04] >> PassGenerator
-  [05] >> Termux Style
-  [06] >> Clear logs
-  [07] >> Termux commands
-  [08] >> Create TermuxBackup
-  [09] >> Restore TermuxBackup
-  [10] >> Reset TermuxPackages
-  [11] >> Termux Banner
-  [12] >> Check Packages
-  [13] >> Add NewExtraKeys
-  [14] >> Exit System
+  [01] >> View MySystem - View my system
+  [02] >> Google Search - Open google in terminal
+  [03] >> Battarey Status - View my battarey status
+  [04] >> PassGenerator - Generate passworld
+  [05] >> Termux Style - Terminal castomization
+  [06] >> Clear logs - Clear terminal logs
+  [07] >> Terminal commands - View terminal commands
+  [08] >> Create TerminalBackup - Create terminal backup
+  [09] >> Restore TerminalBackup - Restore terminal backup
+  [10] >> Reset TerminalPackages - Reset terminal packages
+  [11] >> Terminal Banner - Create castom terminal banner
+  [12] >> Check Packages - check my packages
+  [13] >> Add NewExtraKeys - Add nae extra keys
+  [14] >> Exit System - log out AllHackingTools
   [15] >> Back To MainMenu '| lolcat -p 1.7
