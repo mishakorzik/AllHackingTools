25cf1f16a19d90f5396d1f8ee104f59c1c414b8f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-12 22:49:13 +0300

    Create desingpanel1.sh

---
+++ .desing/desingpanel/desingpanel1.sh
@@ -0,0 +1,16 @@
+echo '
+  [01] >> View MySystem
+  [02] >> Google Search
+  [03] >> Battarey Status
+  [04] >> PassGenerator
+  [05] >> Termux Style
+  [06] >> Clear logs
+  [07] >> Termux commands
+  [08] >> Create TermuxBackup
+  [09] >> Restore TermuxBackup
+  [10] >> Reset TermuxPackages
+  [11] >> Termux Banner
+  [12] >> Check Packages
+  [13] >> Add NewExtraKeys
+  [14] >> Exit System
+  [15] >> Back To MainMenu '| lolcat -p 1.7
