cfaca48113b75bd8edfb77060ef983c7180c989d
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-13 10:35:41 +0300

    Update settingsMenu.py

--- .settings/settingsMenu.py
+++ .settings/settingsMenu.py
@@ -42,9 +42,10 @@ elif(op==7):
  os.system("clear")
  os.system("cd && cd AllHackingTools && bash Logo.sh")
  time.sleep(0.2)
- print("\033[1;31;40mWait A Bit For The Backup To Be Created...")
+ print("\033[1;31;40mRestoring AllHackingTools backup...")
  os.system("cd /sdcard/ && cp -r AllHackingTools /data/data/com.termux/files/home/")
- print("successfully created a backup in: sdcard...")
+ os.system("cd && bash AllHackingTools/.settings/RestoreAllHackingToolsBackup.sh")
+ print("successfully restored backup in: sdcard...")
 elif(op==8):
  os.system("clear")
  os.system("cd && cd AllHackingTools && bash Logo.sh")
