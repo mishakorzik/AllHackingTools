0e93b6bfbba30237b34c3f625ad48b762716a6bc
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-13 22:54:32 +0300

    Update RouterMenu.py

--- Files/RouterMenu.py
+++ Files/RouterMenu.py
@@ -38,7 +38,7 @@ elif(op==6):
  os.system("cd && cd AllHackingTools && cd fim && python fim.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==7):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting utility...")
+ print("\033[1;31;40mQuiting System...")
  sys.exit()
 elif(op==8):
  os.system("cd")
