c629c99926ff1bb51e5656d2fc2bf9a43d30e439
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-13 12:07:06 +0300

    Create ProcessExplorer.sh

---
+++ src/ProcessExplorer.sh
@@ -0,0 +1,638 @@
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+unset LD_LIBRARY_PATH LD_PRELOAD
+PATH=$PATH:/system/bin exec /system/bin/top "$@"
+sleep 0.2
+clear
+clear
+
+
+
+
+
+
+
+
+
+
+
+
