e04ed685d1578bd6d3ca7bb0d5cad424ea3a0b53
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-13 10:33:13 +0300

    Create RestoreAllHackingToolsBackup.sh

---
+++ .settings/RestoreAllHackingToolsBackup.sh
@@ -0,0 +1,32 @@
+cd
+cd
+cd AllHackingTools
+cd Tool
+cp msdc /data/data/com.termux/files/usr/bin/
+cp msdconsole /data/data/com.termux/files/usr/bin/
+cp msdconsoleUPD /data/data/com.termux/files/usr/bin/
+cp msdServer /data/data/com.termux/files/usr/bin/
+cp msd /data/data/com.termux/files/usr/bin/
+cp ms /data/data/com.termux/files/usr/bin/
+cp m /data/data/com.termux/files/usr/bin/
+cp sys /data/data/com.termux/files/usr/bin/
+cp system /data/data/com.termux/files/usr/bin/
+cp View-deleted-activity /data/data/com.termux/files/usr/bin/
+cd
+cd
+cd /data/data/com.termux/files/usr/bin/
+chmod +x msdconsole
+chmod +x msdconsoleUPD
+chmod +x msdc
+chmod +x msdServer
+chmod +x msd
+chmod +x ms
+chmod +x m
+chmod +x sys
+chmod +x system
+chmod +x View-deleted-activity
+ls
+sleep 0.2
+clear
+cd 
+cd 
