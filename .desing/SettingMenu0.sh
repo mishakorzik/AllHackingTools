f9ec7bcb39e367b0daba011c9e98ef848c37f359
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-13 10:26:03 +0300

    Update SettingMenu0.sh

--- .desing/SettingMenu0.sh
+++ .desing/SettingMenu0.sh
@@ -5,5 +5,7 @@ echo '
 [ 04 ] >> Special Opportunities
 [ 05 ] >> View My Activity
 [ 06 ] >> Delete My Activity
-[ 07 ] >> Exit System
-[ 08 ] >> Back To MainMenu '| lolcat -p 1.7
+[ 07 ] >> Restore AllHackingTools backup
+[ 08 ] >> Create AllHackingTools backup
+[ 09 ] >> Exit System - log out AllHackingTools
+[ 10 ] >> Back To MainMenu - Back To Main Menu '| lolcat -p 1.8
