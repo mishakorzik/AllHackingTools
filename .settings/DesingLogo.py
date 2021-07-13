f1ebe7bda014c732c3061daa0deff9a7457f2c1c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-13 22:41:08 +0300

    Update DesingLogo.py

--- .settings/DesingLogo.py
+++ .settings/DesingLogo.py
@@ -10,8 +10,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mDesing Logo 1")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mDesing Logo 2")
 print("  \033[1;34m[ 03 ] >> \033[1;36;40mDesing Logo 3")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mExit System")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mDesing Logo 4")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System")
+print("  \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Sett1Ngs: "))
 
@@ -25,10 +26,13 @@ elif(op==3):
  os.system("clear")
  os.system("cd && cd AllHackingTools && mv Logo.sh /data/data/com.termux/files/home/AllHackingTools/.temp/DesingTemp2/ && cd && cd AllHackingTools && cd .settings && cd desing2 && mv Logo.sh /data/data/com.termux/files/home/AllHackingTools/ && cd && cd AllHackingTools && cd .temp && cd DesingTemp2 && mv Logo.sh /data/data/com.termux/files/home/AllHackingTools/.settings/desing2/ && cd && cd AllHackingTools && bash .settings/Applined.sh")
 elif(op==4):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && mv Logo.sh /data/data/com.termux/files/home/AllHackingTools/.temp/DesingTemp3/ && cd && cd AllHackingTools && cd .settings && cd desing3 && mv Logo.sh /data/data/com.termux/files/home/AllHackingTools/ && cd && cd AllHackingTools && cd .temp && cd DesingTemp3 && mv Logo.sh /data/data/com.termux/files/home/AllHackingTools/.settings/desing3/ && cd && cd AllHackingTools && bash .settings/Applined.sh")
+elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==5):
+elif(op==6):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
