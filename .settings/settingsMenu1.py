d1935b8c2165a66f613132a9b0a7ffe16f99820b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-16 10:11:16 +0300

    Update settingsMenu1.py

--- .settings/settingsMenu1.py
+++ .settings/settingsMenu1.py
@@ -21,7 +21,7 @@ elif(op==3):
  os.system("cd && cd AllHackingTools && cd src && mv AnimationLoad1.sh /data/data/com.termux/files/home/AllHackingTools/.temp/ && cd && cd AllHackingTools && cd .settings && mv AnimationLoad1.sh /data/data/com.termux/files/home/AllHackingTools/src/ && cd && cd AllHackingTools && cd .temp && mv AnimationLoad1.sh /data/data/com.termux/files/home/AllHackingTools/.settings/ && cd && cd AllHackingTools && bash .settings/Applined.sh")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd .settings && python2 SpecialOpportunities.py")
+ os.system("cd && cd AllHackingTools && cd .settings && python2 SpecialOpportunities1.py")
 elif(op==5):
  os.system("clear")
  os.system("cd && nano .zsh_history")
