4c90d343d845e34d133516b7e3935055cd1b5c48
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-16 10:32:36 +0300

    Update SpecialMenu1.sh

--- .desing/desing/SpecialMenu1.sh
+++ .desing/desing/SpecialMenu1.sh
@@ -1,9 +1,9 @@
 echo '
-[01] >> Quick Start - on/off
-[02] >> Servers Setting
-[03] >> New ExtraKeys0
-[04] >> New ExtraKeys1
-[05] >> Add AllHackingTool to startup
-[06] >> View MySystem Process - ProcessExplorer
-[07] >> Exit System - log out AllHackingTool
-[08] >> Back To MainMenu - go to the home menu '| lolcat -p 1.5
+  [01] >> Quick Start - on/off
+  [02] >> Servers Setting
+  [03] >> New ExtraKeys0
+  [04] >> New ExtraKeys1
+  [05] >> Add AllHackingTool to startup
+  [06] >> View MySystem Process - ProcessExplorer
+  [07] >> Exit System - log out AllHackingTool
+  [08] >> Back To MainMenu - go to the home menu '| lolcat -p 1.5
