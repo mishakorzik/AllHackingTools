c5edab6ca46ba83d67a350bc95b29ee2184b4946
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-16 23:16:20 +0300

    Update MailMenu.py

--- Files/MailMenu.py
+++ Files/MailMenu.py
@@ -10,8 +10,9 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mMailHack - Email hacker")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mPwnedOrnot - Tool for find passworld email")
 print("  \033[1;34m[ 03 ] >> \033[1;36;40mEmailPySpam - A python 3+ program to spam emails and more options!")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mExit System")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mGmailHack - A python Easy email hacker")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System - log out AllHackingTools")
+print("  \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Ma1lHacK: "))
 
@@ -25,9 +26,12 @@ elif(op==3):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd emailpyspam && cd main && python3 emailspam.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==4):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Gmail-Hack && python3 Mail-Hack.py && cd && cd AllHackingTools && python2 MainMenu.py")
+elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
-elif(op==5):
+elif(op==6):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
