f7d5061a07be5578b7fc4ee798a5c346f7cfc9bd
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-19 16:50:14 +0300

    Create SocialFinderFiles.sh

---
+++ Files/SocialFinderFiles.sh
@@ -0,0 +1,14 @@
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/sherlock-project/sherlock.git
+cd sherlock
+python3 -m pip install -r requirements.txt
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/xHak9x/finduser
+cd
+cd
+cd AllHackingTools
+
