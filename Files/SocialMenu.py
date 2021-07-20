35c8d5add75b6c3e0e689da2cf5f0b96c276997b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-20 12:25:55 +0300

    Update SocialMenu.py

--- Files/SocialMenu.py
+++ Files/SocialMenu.py
@@ -9,8 +9,11 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mInstaHack - Best Tool For instagram bruteforce hacking Tool")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mFacebook - Bruteforce attack on Facebook account using python")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit System - Log out AllHackingTools")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mFacebook - Bruteforce attack on Facebook account using python")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mFacebook - Bruteforce attack on Facebook account using python")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mFacebook - Bruteforce attack on Facebook account using python")
+print("  \033[1;34m[ 06 ] >> \033[1;36;40mExit System - Log out AllHackingTools")
+print("  \033[1;34m[ 07 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Soc1alF1sh: "))
 
@@ -21,9 +24,18 @@ elif(op==2):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd Facebook-BruteForce && python3 fb.py or python fb2.py")
 elif(op==3):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Files && bash SherlockUser.sh")
+elif(op==4):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Files && bash FindUserName.sh")
+elif(op==5):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Facebook-BruteForce && python3 fb.py or python fb2.py")
+elif(op==6):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System..")
-elif(op==4):
+elif(op==7):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
