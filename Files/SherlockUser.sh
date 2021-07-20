e764b69fb8bef77f1adcc269dda7c9f682f90108
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-20 12:23:03 +0300

    Create SherlockUser.sh

---
+++ Files/SherlockUser.sh
@@ -0,0 +1,22 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+clear
+cd
+cd AllHackingTools
+bash Logo.sh
+cd sherlock
+echo ""
+echo -e $b">>>"$w" Write the UserName: "$g"Sherlock"$w
+read NameOrUser
+sleep 0.1
+echo -e $b">>>"$w" Please wait a moment: "$g"Sherlock"$w
+sleep 0.8
+clear
+python3 sherlock.py $NameOrUser
+cd
+cd
+cd AllHackingTools
+python3 src/Timer2.py
