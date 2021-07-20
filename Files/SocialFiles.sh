d23dcdbab74c69a41e2796fccdedb3a881ae17ec
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-20 12:11:42 +0300

    Update SocialFiles.sh

--- Files/SocialFiles.sh
+++ Files/SocialFiles.sh
@@ -15,3 +15,17 @@ python3 -m pip install mechanize
 cd
 cd
 cd AllHackingTools
+git clone https://github.com/sherlock-project/sherlock.git
+cd sherlock
+python3 -m pip install -r requirements.txt
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/xHak9x/finduser
+cd
+cd
+cd AllHackingTools
+git clone https://github.com/mishakorzik/UserFinder
+cd
+cd
+cd AllHackingTools
