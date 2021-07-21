3ad4222c2dba7aedbb9d98fcbd2317aa8dbd2bd3
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-21 14:32:32 +0300

    Update PackagesInstaller.sh

--- Files/PackagesInstaller.sh
+++ Files/PackagesInstaller.sh
@@ -247,7 +247,6 @@ sleep 3 & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '\'
 echo ""
 
 apt install apache2
-apt install Apache2
 apt install openssl -y
 apt install python-dev -y
 apt install python3 -y
