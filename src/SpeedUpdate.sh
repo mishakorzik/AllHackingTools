33d0064152a0605c0b26ab42cffa439a4f77ab38
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-21 14:06:18 +0300

    Create SpeedUpdate.sh

---
+++ src/SpeedUpdate.sh
@@ -0,0 +1,40 @@
+cd
+cd
+cd /data/data/com.termux/files/usr/bin/
+rm -rf msdc
+rm -rf msdconsole
+rm -rf msdconsoleUPD
+rm -rf msdServer
+rm -rf msd
+rm -rf ms
+rm -rf m
+rm -rf sys
+rm -rf system
+rm -rf View-deleted-activity
+cd
+cd
+cd AllHackingTools
+cd Tool
+cp msdc /data/data/com.termux/files/usr/bin/
+cp msdconsole /data/data/com.termux/files/usr/bin/
+cp msdconsoleUPD /data/data/com.termux/files/usr/bin/
+cp msdServer /data/data/com.termux/files/usr/bin/
+cp msd /data/data/com.termux/files/usr/bin/
+cp ms /data/data/com.termux/files/usr/bin/
+cp m /data/data/com.termux/files/usr/bin/
+cp sys /data/data/com.termux/files/usr/bin/
+cp system /data/data/com.termux/files/usr/bin/
+cp View-deleted-activity /data/data/com.termux/files/usr/bin/
+cd
+cd
+cd /data/data/com.termux/files/usr/bin/
+chmod +x msdconsole
+chmod +x msdconsoleUPD
+chmod +x msdc
+chmod +x msdServer
+chmod +x msd
+chmod +x ms
+chmod +x m
+chmod +x sys
+chmod +x system
+chmod +x View-deleted-activity
