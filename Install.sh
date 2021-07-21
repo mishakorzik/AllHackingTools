ce7c1a5be12aa87ac5a79f094ac1de339922e398
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-21 15:20:07 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,10 +1,20 @@
-clear
-
 g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
 
+clear
+ping -c 1 google.com > /dev/null 2>&1
+if [[ "$?" == 0 ]]; then
+echo ""
+echo -e "$green[+]-[Internet Connection]............[ True ]"
+sleep 1.5
+echo -e "$default"
 apt install python 
 apt install python2 
 apt install pip
@@ -14,19 +24,6 @@ pip2 install requests
 cd
 cd
 cd AllHackingTools
-
-red='\e[1;31m'
-default='\e[0m'
-yellow='\e[0;33m'
-orange='\e[38;5;166m'
-green='\033[92m'
-
-ping -c 1 google.com > /dev/null 2>&1
-if [[ "$?" == 0 ]]; then
-echo ""
-echo -e "$green[+]-[Internet Connection]............[ True ]"
-sleep 1.5
-echo -e "$default"
 python2 src/InstallMenu.py
 else
 echo ""
