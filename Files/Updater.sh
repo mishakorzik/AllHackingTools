9955d58616b5d8ea8aa0607cffe02672b421c2da
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-21 13:15:12 +0300

    Update Updater.sh

--- Files/Updater.sh
+++ Files/Updater.sh
@@ -4,17 +4,36 @@ b="\033[1;34m"
 w="\033[0m"
 o="\033[1;33m"
 
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+echo -e "$yellow  ____ ___            .___                      "
+echo -e "$yellow |    |   \______   __| _/____ _/  |_  ____     "
+echo -e "$yellow |    |   /\____ \ / __ |\__  \\   __\/ __ \    "
+echo -e "$yellow |    |  / |  |_> > /_/ | / __ \|  | \  ___/    "
+echo -e "$yellow |______/  |   __/\____ |(____  /__|  \___  >   "
+echo -e "$yellow           |__|        \/     \/          \/    "
+echo ""
+ping -c 1 google.com > /dev/null 2>&1
+if [[ "$?" == 0 ]]; then
+echo ""
+echo -e "$green[+]-[Internet Connection]............[ True ]"
+sleep 1.5
+else
+echo ""
+echo -e "$red[-]-[Internet Connection].........[ False ]"
+echo ""
+sleep 99999
+fi
+
 cd
 rm -rf AutoUpdateMyTools
 cd 
 git clone https://github.com/mishakorzik/AutoUpdateMyTools 
 cd AutoUpdateMyTools
-echo -e $w"["$o"WARN"$w"]"$b"verifining settings!"$w
-sleep 0.2
-echo -e $w"["$o"WARN"$w"]"$b"I don't close termux app!"$w
-sleep 1
-echo -e $w"["$g"INFO"$w"]"$b"Succesfull verifined!"$w
-sleep 0.9
 cd
 cd
 cd /data/data/com.termux/files/usr/share/figlet
