94478f1b051c42407a8844f1d1304a181b906276
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-22 23:04:14 +0300

    Update PhishingMenu.py

--- Files/PhishingMenu.py
+++ Files/PhishingMenu.py
@@ -16,9 +16,10 @@ print("  \033[1;34m[ 06 ] >> \033[1;36;40mUserRecon - find people from social")
 print("  \033[1;34m[ 07 ] >> \033[1;36;40mMask Phish - An masking phishing url")
 print("  \033[1;34m[ 08 ] >> \033[1;36;40mSeeker - Accurately Locate Smartphones")
 print("  \033[1;34m[ 09 ] >> \033[1;36;40mAIOPhish - A Social Toolkit for phishing")
-print("  \033[1;34m[ 10 ] >> \033[1;36;40mISeeYou- Find the exact location of the users")
-print("  \033[1;34m[ 11 ] >> \033[1;36;40mExit System - log out AllHackingTools")
+print("  \033[1;34m[ 10 ] >> \033[1;36;40mISeeYou - Find the exact location of the users")
+print("  \033[1;34m[ 11 ] >> \033[1;36;40mHiddenEye - ")
 print("  \033[1;34m[ 12 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 13 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Phish1ng: "))
 
@@ -58,10 +59,13 @@ elif(op==10):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd I-See-You && ./ISeeYou.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==11):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd HiddenEye && python HiddenEye.py && cd && cd AllHackingTools && python2 MainMenu.py")
+elif(op==12):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==12):
+elif(op==13):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
