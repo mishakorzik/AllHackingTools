2ba574a43b4c749f86f0eafe79796c1768a2d6a1
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 11:54:51 +0300

    Update Dracula.sh

--- Themes/Dracula.sh
+++ Themes/Dracula.sh
@@ -5,3 +5,4 @@ rm -rf /data/data/com.termux/files/usr/etc/motd
 cd ~/AllHackingTools/Themes/.object ; cp -r .colors.properties3 ~/.termux/colors.properties
 tput sgr0 #reset attributes
 tput op #reset color
+am broadcast --user 0 -a com.termux.app.reload_style com.termux > /dev/null
