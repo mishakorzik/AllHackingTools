9181c548f222038721d271d1a63b7cc441c98fba
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 11:55:05 +0300

    Update GoogleDark.sh

--- Themes/GoogleDark.sh
+++ Themes/GoogleDark.sh
@@ -3,3 +3,4 @@ cd
 rm -rf ~/.termux/colors.properties
 rm -rf /data/data/com.termux/files/usr/etc/motd
 cd ~/AllHackingTools/Themes/.object ; cp -r .colors.properties7 ~/.termux/colors.properties
+am broadcast --user 0 -a com.termux.app.reload_style com.termux > /dev/null
