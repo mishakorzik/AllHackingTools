ec4730f5974a9cd1254d0262a977a1f032d3a94f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 22:30:45 +0300

    Update Uninstall.sh

--- Uninstall/Uninstall.sh
+++ Uninstall/Uninstall.sh
@@ -4,18 +4,24 @@ b="\033[1;34m"
 w="\033[0m"
 o="\033[1;33m"
 
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+DEFAULT_FG="$(printf '\033[39m')"  DEFAULT_BG="$(printf '\033[49m')" YELLOW="$(printf '\e[0;33m')"
+
 red='\e[1;31m'
 default='\e[0m'
 yellow='\e[0;33m'
 orange='\e[38;5;166m'
 green='\033[92m'
 
-echo -e "$yellow  ____ ___      .__                 __         .__  .__     "
-echo -e "$yellow |    |   \____ |__| ____   _______/  |______  |  | |  |    "
-echo -e "$yellow |    |   /    \|  |/    \ /  ___/\   __\__  \ |  | |  |    "
-echo -e "$yellow |    |  /   |  \  |   |  \\___ \   |  |  / __ \|  |_|  |__ "
-echo -e "$yellow |______/|___|  /__|___|  /____  > |__| (____  /____/____/  "
-echo -e "$yellow              \/        \/     \/            \/             "
+echo "${YELLOW}  ____ ___      .__                 __         .__  .__     "
+echo "${YELLOW} |    |   \____ |__| ____   _______/  |______  |  | |  |    "
+echo "${YELLOW} |    |   /    \|  |/    \ /  ___/\   __\__  \ |  | |  |    "
+echo "${YELLOW} |    |  /   |  \  |   |  \\___ \   |  |  / __ \|  |_|  |__ "
+echo "${YELLOW} |______/|___|  /__|___|  /____  > |__| (____  /____/____/  "
+echo "${YELLOW}              \/        \/     \/            \/             "
 echo ""
 echo -e $w"["$o"SYSTEM"$w"]"$w" Press enter to uninstall AllHackingTools"$w
 read a1
