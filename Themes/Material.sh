fbaccf2e47c2a059ec07a495a083c4f2eb7de63b
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 11:58:54 +0300

    Update Material.sh

--- Themes/Material.sh
+++ Themes/Material.sh
@@ -1,7 +1,8 @@
 cd
-apt install toilet figlet exa wget ruby 
+cd
 rm -rf ~/.termux/colors.properties
 rm -rf /data/data/com.termux/files/usr/etc/motd
 cd ~/AllHackingTools/Themes/.object ; cp -r .colors.properties4 ~/.termux/colors.properties
 tput sgr0 #reset attributes
 tput op #reset color
+am broadcast --user 0 -a com.termux.app.reload_style com.termux > /dev/null
