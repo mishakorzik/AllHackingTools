df22d2e197e63b67567224cb218fc4126c6164ef
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 11:49:39 +0300

    Update Smyck.sh

--- Themes/Smyck.sh
+++ Themes/Smyck.sh
@@ -1,7 +1,7 @@
-tput sgr0 #reset attributes
-tput op #reset color
 cd
 cd
 rm -rf ~/.termux/colors.properties
 rm -rf /data/data/com.termux/files/usr/etc/motd
 cd ~/AllHackingTools/Themes/.object ; cp -r .colors.properties2 ~/.termux/colors.properties
+tput sgr0 #reset attributes
+tput op #reset color
