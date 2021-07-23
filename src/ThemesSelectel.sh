a0b0096ada8f4758aad7787c2a6913711f2aee24
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 13:04:47 +0300

    Update ThemesSelectel.sh

--- src/ThemesSelectel.sh
+++ src/ThemesSelectel.sh
@@ -20,6 +20,9 @@ echo "|                           |"
 echo "| ${RED}While   1/2/3/4/5/6/7/8: ${BLUE} |"
 echo "----  ----------       ------"
 read numb
+echo -n "${BLUE}[${RED}!]${BLUE} Loading Settings...
+sleep 0.2
+echo -n "${BLUE}[${RED}!]${BLUE} Applined Succesful..!
 if [ $numb = "1" ]
 then
         cd
