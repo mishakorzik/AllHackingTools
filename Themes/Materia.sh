dfbaae12705ba2237934189f3177c485a57d3046
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 11:50:47 +0300

    Update Materia.sh

--- Themes/Materia.sh
+++ Themes/Materia.sh
@@ -1,8 +1,7 @@
-tput sgr0 #reset attributes
-tput op #reset color
 cd
 cd
-apt install toilet figlet exa wget ruby 
 rm -rf ~/.termux/colors.properties
 rm -rf /data/data/com.termux/files/usr/etc/motd
 cd ~/AllHackingTools/Themes/.object ; cp -r .colors.properties6 ~/.termux/colors.properties
+tput sgr0 #reset attributes
+tput op #reset color
