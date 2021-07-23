8c42c228cd864e66bbb7d1ce49390d02bfe77b1e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-23 22:13:21 +0300

    Update Applined.sh

--- .settings/Applined.sh
+++ .settings/Applined.sh
@@ -4,6 +4,6 @@ b="\033[1;34m"
 w="\033[0m"
 o="\033[1;33m"
 
-echo -e $w"["$g"INFO"$w"]"$b"SuccesFull Applined Settings!"$w
+echo -e $w"["$g"INFO"$w"]"$b"Succesful Applined Settings!"$w
 sleep 0.1
 echo -e $w"["$o"WARN"$w"]"$b"Please Start AllHackingTools To View!"$w
