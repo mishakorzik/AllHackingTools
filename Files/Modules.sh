1cd8f1b23f9e98b865a10dea5eb565cacff4185c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-24 19:19:14 +0300

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -51,51 +51,9 @@ bash Files/OtherFiles.sh
 cd
 cd
 cd AllHackingTools
-cd Tool
-cp msdc /usr/local/bin/
-cp msdconsole /usr/local/bin/
-cp msdconsoleUPD /usr/local/bin/
-cp msdServer /usr/local/bin/
-cp msd /usr/local/bin/
-cp ms /usr/local/bin/
-cp m /usr/local/bin/
-cp sys /usr/local/bin/
-cp system /usr/local/bin/
-cp View-deleted-activity /usr/local/bin/
-cp theme /usr/local/bin/
-cp Theme /usr/local/bin/
-cp msdc /data/data/com.termux/files/usr/bin/
-cp msdconsole /data/data/com.termux/files/usr/bin/
-cp msdconsoleUPD /data/data/com.termux/files/usr/bin/
-cp msdServer /data/data/com.termux/files/usr/bin/
-cp msd /data/data/com.termux/files/usr/bin/
-cp ms /data/data/com.termux/files/usr/bin/
-cp m /data/data/com.termux/files/usr/bin/
-cp sys /data/data/com.termux/files/usr/bin/
-cp system /data/data/com.termux/files/usr/bin/
-cp View-deleted-activity /data/data/com.termux/files/usr/bin/
-cp Theme /data/data/com.termux/files/usr/bin/
-cp theme /data/data/com.termux/files/usr/bin/
+cd Files
+bash TypeTermuxAndLinux.sh
 cd
-cd
-cd /data/data/com.termux/files/usr/bin/
-cd /usr/local/bin/
-chmod +x msdconsole
-chmod +x msdconsoleUPD
-chmod +x msdc
-chmod +x msdServer
-chmod +x msd
-chmod +x ms
-chmod +x m
-chmod +x sys
-chmod +x system
-chmod +x View-deleted-activity
-chmod +x Theme
-chmod +x theme
-ls
-sleep 0.2
-clear
-cd 
 cd 
 Rootkite-delete-qiq
 pkg upgrade
