340e00620a087f198727ca7024af7c12f710ccad
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-31 20:42:48 +0300

    Create MenuOps5.sh

---
+++ src/MenuOps5.sh
@@ -0,0 +1,44 @@
+echo '
+  [01] >> Information Gathering: 
+   ├ Collects information from database
+   ├ Find More Information
+   ├ More Information In User
+   │
+  [02] >> Exploitation Tools:
+   ├ Selection of tools for operation and hacking
+   ├ Tools for operations 
+   ├ blackmail utilities 
+   ├ Utilities for human use
+   │
+  [03] >> Sniffing and Spoofing:
+   ├ Tools for forgery of data and databases
+   ├ Tools for crawling data
+   │
+  [>]┐
+    [04] >> Web Attack Tools:
+     ├ Tools for hacking websites and servers
+     ├ Utilities for hacking the admin panel of sites
+     │
+    [05] >> Cam Hacking Tools:
+     ├ Tools for hacking cams and front camera
+    [06] >> Remote Trojan RAT:
+     └ Utilities for creating RAT virus
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
+  [07] >> SQL injection Tool:  -  Tools for creating viruses
+  [08] >> SocialMedia Hacking:  -  Tools for hacking social networks
+  [09] >> SMS spaming Tools:  -  Utilities for spam and anonymous SMS
+  [10] >> Vulnerability Analysis:  -  Systems for vulnerability analysis
+  [11] >> DarkSearch Tool:  -  Tools for Secret and Onion search 
+  [12] >> Phishing And IpHack:  -  Tools for phishing and fake websites
+  [13] >> Passworld Attack:  -  Utilities for cracking passwords
+  [14] >> Wordlist Generator:  -  Tools for Genering Worldlist
+  [15] >> XSS Attack Tools:  -  Utilities for Attacking XSS
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣ 
+  [16] >> Other tools:  -  Separate and other tools for hacking
+  [17] >> Terminal Panel:  -  Termux control and special features
+  [18] >> System Settings:  -  AllHackingTools control and special features
+  [19] >> System License:  -  View the license in AllHackingTools
+  [20] >> Update System:  -  Update system and all tools & menu 
+  [21] >> About System:  -  View system information and developer
+  [22] >> Exit System:  -  Log out of the AllHackingTools system '|lolcat -p 2.0
+
