e4d079ca74462f92702eb02ec58b950ce9fb7a80
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-31 21:06:06 +0300

    Update MenuOps5.sh

--- src/MenuOps5.sh
+++ src/MenuOps5.sh
@@ -1,4 +1,5 @@
 echo '
+   ╔[NOTICE] Steeper Tools
   [01] >> Information Gathering: 
    ├ Collects information from database
    ├ Find More Information
@@ -18,27 +19,55 @@ echo '
     [04] >> Web Attack Tools:
      ├ Tools for hacking websites and servers
      ├ Utilities for hacking the admin panel of sites
+     ├ And DDOS websites
      │
     [05] >> Cam Hacking Tools:
      ├ Tools for hacking cams and front camera
     [06] >> Remote Trojan RAT:
      └ Utilities for creating RAT virus
       ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
-  [07] >> SQL injection Tool:  -  Tools for creating viruses
-  [08] >> SocialMedia Hacking:  -  Tools for hacking social networks
-  [09] >> SMS spaming Tools:  -  Utilities for spam and anonymous SMS
-  [10] >> Vulnerability Analysis:  -  Systems for vulnerability analysis
-  [11] >> DarkSearch Tool:  -  Tools for Secret and Onion search 
-  [12] >> Phishing And IpHack:  -  Tools for phishing and fake websites
-  [13] >> Passworld Attack:  -  Utilities for cracking passwords
-  [14] >> Wordlist Generator:  -  Tools for Genering Worldlist
-  [15] >> XSS Attack Tools:  -  Utilities for Attacking XSS
-      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣ 
-  [16] >> Other tools:  -  Separate and other tools for hacking
-  [17] >> Terminal Panel:  -  Termux control and special features
-  [18] >> System Settings:  -  AllHackingTools control and special features
-  [19] >> System License:  -  View the license in AllHackingTools
-  [20] >> Update System:  -  Update system and all tools & menu 
-  [21] >> About System:  -  View system information and developer
-  [22] >> Exit System:  -  Log out of the AllHackingTools system '|lolcat -p 2.0
+         ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
+   ╔[NOTICE] Useful Tools 
+  [07] >> SQL injection Tool:
+   ├ Tools for creating viruses
+   ├ And cracking devices
+  [08] >> SocialMedia Hacking:
+   ├ Tools for hacking social networks
+   ├ And Bruteforce social networks
+   │
+  [>]┐
+    [09] >> SMS spaming Tools:
+     ├ Utilities for sms bombing
+     ├ And Send anonymous SMS
+    [10] >> Vulnerability Analysis:
+     ├ Systems for vulnerability analysis
+     ├ And testing devices
+    [11] >> DarkSearch Tool:
+     ├ Tools for Secret and Onion search 
+     ├ And Search in DarkNet & DeepWeb
+    [12] >> Phishing And IpHack:
+     ├ Tools for phishing and fake websites
+     ├ And Crawling email and passwords
+     ├ And Tools for creting fake websites
+     │
+   ┌[<]
+  [13] >> Passworld Attack:
+   ├ Utilities for cracking passwords
+  [14] >> Wordlist Generator: 
+   ├ Tools for Genering Worldlist
+  [15] >> XSS Attack Tools:
+   └ Utilities for Attacking XSS
+      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
+         ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  
+  [16] >> Other tools: 
+   ├ Separate and other tools for hacking
+  [17] >> Terminal Panel: 
+   ├ Termux control and special features
+   │
+  [SKIP]┐
+      ┌[18] >> System Settings:  -  AllHackingTools control and special features
+      │[19] >> System License:  -  View the license in AllHackingTools
+  [-]─┘[20] >> Update System:  -  Update system and all tools & menu 
+       [21] >> About System:  -  View system information and developer
+       [22] >> Exit System:  -  Log out of the AllHackingTools system '|lolcat -p 2.0
 
