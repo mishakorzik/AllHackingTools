807e1d4c1c80b0b421b7c738e660865bd970a74f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-03 22:42:41 +0300

    Create an4.sh

---
+++ .anim/preview/an4.sh
@@ -0,0 +1,13 @@
+echo "
+    [01] Server: Succesful
+     │
+     ├[02] DNS: Succesful
+     ├[03] System: Succesful
+     │
+    [>]┐
+       │
+      [05] Configuration:
+      [06] Response:
+       │
+     [START]
+"|lolcat -p 1.5
