37360bb8f9b8aa120eb843fcacd714eeb978a350
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-03 23:11:26 +0300

    Update AnimationLoad2.sh

--- src/AnimationLoad2.sh
+++ src/AnimationLoad2.sh
@@ -6,102 +6,102 @@ w="\033[0m"
 o="\033[1;33m"
 printf "\e[1;92m"
 
-printf "\n#000000000000000000000 Loading .\n"
+printf "\n#aaaaaaaaaaaaaaaaaaaaa Loading .\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n#000000000000000000000 Loading ..\n"
+printf "\n#aaaaaaaaaaaaaaaaaaaaa Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n##00000000000000000000 Loading ...\n"
+printf "\n##aaaaaaaaaaaaaaaaaaaa Loading ...\n"
 sleep 0.01
 clear
-printf "\n##00000000000000000000 Loading .\n"
+printf "\n##aaaaaaaaaaaaaaaaaaaa Loading .\n"
 sleep 0.05
 clear
-printf "\n###0000000000000000000 Loading ..\n"
+printf "\n###aaaaaaaaaaaaaaaaaaa Loading ..\n"
 clear
-printf "\n###0000000000000000000 Loading ...\n"
+printf "\n###aaaaaaaaaaaaaaaaaaa Loading ...\n"
 clear
-printf "\n####000000000000000000 Loading .\n"
+printf "\n####aaaaaaaaaaaaaaaaaa Loading .\n"
 sleep 0.01
 clear
-printf "\n####000000000000000000 Loading ..\n"
+printf "\n####aaaaaaaaaaaaaaaaaa Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n#####00000000000000000 Loading ...\n"
+printf "\n#####aaaaaaaaaaaaaaaaa Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n#####00000000000000000 Loading .\n"
+printf "\n#####aaaaaaaaaaaaaaaaa Loading .\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.02
 clear
-printf "\n######0000000000000000 Loading ..\n"
+printf "\n######aaaaaaaaaaaaaaaa Loading ..\n"
 sleep 0.02
 clear
-printf "\n#######000000000000000 Loading ...\n"
+printf "\n#######aaaaaaaaaaaaaaa Loading ...\n"
 sleep 0.01
 clear
-printf "\n#######000000000000000 Loading .\n"
+printf "\n#######aaaaaaaaaaaaaaa Loading .\n"
 sleep 0.01
 clear
-printf "\n#########0000000000000 Loading ..\n"
+printf "\n#########aaaaaaaaaaaaa Loading ..\n"
 clear
-printf "\n##########000000000000 Loading ...\n"
+printf "\n##########aaaaaaaaaaaa Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n###########00000000000 Loading .\n"
+printf "\n###########aaaaaaaaaaa Loading .\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n###########00000000000 Loading ..\n"
+printf "\n###########aaaaaaaaaaa Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n############0000000000 Loading ...\n"
+printf "\n############aaaaaaaaaa Loading ...\n"
 sleep 0.03
 clear
-printf "\n#############000000000 Loading .\n"
+printf "\n#############aaaaaaaaa Loading .\n"
 sleep 0.01
 clear
-printf "\n##############00000000 Loading ..\n"
+printf "\n##############aaaaaaaa Loading ..\n"
 sleep 0.05
 clear
-printf "\n###############0000000 Loading ...\n"
+printf "\n###############aaaaaaa Loading ...\n"
 clear
-printf "\n################000000 Loading .\n"
+printf "\n################aaaaaa Loading .\n"
 sleep 0.005
 clear
-printf "\n################000000 Loading ..\n"
+printf "\n################aaaaaa Loading ..\n"
 sleep 0.01
 clear
-printf "\n#################00000 Loading ...\n"
+printf "\n#################aaaaa Loading ...\n"
 sleep 0.01
 clear
-printf "\n#################00000 Loading .\n"
+printf "\n#################aaaaa Loading .\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n##################0000 Loading ..\n"
+printf "\n##################aaaa Loading ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
 help
-printf "\n###################000 Loading ...\n"
+printf "\n###################aaa Loading ...\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n###################000 Loading .\n"
+printf "\n###################aaa Loading .\n"
 sleep 0.006
 clear
-printf "\n###################000 Loading ..\n"
+printf "\n###################aaa Loading ..\n"
 sleep 0.006
 clear
-printf "\n####################00 Loading ...\n"
+printf "\n####################aa Loading ...\n"
 leep 0.1
 clear
 printf "\n###################### DONE!\n"
