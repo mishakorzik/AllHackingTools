1a586658bdf2a486a11e05a47c99b430306bda0f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-03 22:47:23 +0300

    Create PlayAnim0.sh

---
+++ .anim/PlayAnim0.sh
@@ -0,0 +1,27 @@
+cd
+cd
+cd AllHackingTools
+cd .anim
+clear
+bash an1.sh
+sleep 0.4
+clear
+bash an2.sh
+sleep 0.2
+clear
+bash an3.sh
+sleep 0.4
+clear
+bash an4.sh
+sleep 0.2
+clear
+bash an5.sh
+sleep 0.4
+clear
+bash an6.sh
+sleep 0.4
+clear
+
+
+
+
