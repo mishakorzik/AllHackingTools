29293bca8a07659b11b5d5bc7298d6b2f4fd6129
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-03 23:08:20 +0300

    Create AnimationLoad2.sh

---
+++ src/AnimationLoad2.sh
@@ -0,0 +1,118 @@
+clear
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+printf "\e[1;92m"
+
+printf "\n#000000000000000000000 Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n#000000000000000000000 Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n##00000000000000000000 Loading ...\n"
+sleep 0.01
+clear
+printf "\n##00000000000000000000 Loading .\n"
+sleep 0.05
+clear
+printf "\n###0000000000000000000 Loading ..\n"
+clear
+printf "\n###0000000000000000000 Loading ...\n"
+clear
+printf "\n####000000000000000000 Loading .\n"
+sleep 0.01
+clear
+printf "\n####000000000000000000 Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n#####00000000000000000 Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n#####00000000000000000 Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.02
+clear
+printf "\n######0000000000000000 Loading ..\n"
+sleep 0.02
+clear
+printf "\n#######000000000000000 Loading ...\n"
+sleep 0.01
+clear
+printf "\n#######000000000000000 Loading .\n"
+sleep 0.01
+clear
+printf "\n#########0000000000000 Loading ..\n"
+clear
+printf "\n##########000000000000 Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n###########00000000000 Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n###########00000000000 Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n############0000000000 Loading ...\n"
+sleep 0.03
+clear
+printf "\n#############000000000 Loading .\n"
+sleep 0.01
+clear
+printf "\n##############00000000 Loading ..\n"
+sleep 0.05
+clear
+printf "\n###############0000000 Loading ...\n"
+clear
+printf "\n################000000 Loading .\n"
+sleep 0.005
+clear
+printf "\n################000000 Loading ..\n"
+sleep 0.01
+clear
+printf "\n#################00000 Loading ...\n"
+sleep 0.01
+clear
+printf "\n#################00000 Loading .\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n##################0000 Loading ..\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+help
+printf "\n###################000 Loading ...\n"
+printf "\nPlease wait a moment ..!\n"
+sleep 0.01
+clear
+printf "\n###################000 Loading .\n"
+sleep 0.006
+clear
+printf "\n###################000 Loading ..\n"
+sleep 0.006
+clear
+printf "\n####################00 Loading ...\n"
+leep 0.1
+clear
+printf "\n###################### DONE!\n"
+cd
+cd 
+cd AllHackingTools 
+bash src/CheckWifi.sh
+sleep 2
+clear
+sleep 0.1
+cd
+cd
+cd AllHackingTools
+python3 .check/Configuration.py
