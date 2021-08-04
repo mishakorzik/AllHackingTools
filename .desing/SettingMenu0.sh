ecaf2e268079bc34147cc7775ad3cc2b7d32516c
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-04 10:23:13 +0300

    Update SettingMenu0.sh

--- .desing/SettingMenu0.sh
+++ .desing/SettingMenu0.sh
@@ -2,10 +2,11 @@ echo '
    [ 01 ] >> Logo Setting - d/n
    [ 02 ] >> Menu Setting - d/n
    [ 03 ] >> Vibration Setting - on/off
-   [ 04 ] >> Special Opportunities
-   [ 05 ] >> View My Activity
-   [ 06 ] >> Delete My Activity
-   [ 07 ] >> Restore AllHackingTools backup
-   [ 08 ] >> Create AllHackingTools backup
-   [ 09 ] >> Exit System - log out AllHackingTools
-   [ 10 ] >> Back To MainMenu - Back To Main Menu '| lolcat -p 1.8
+   [ 04 ] >> Default Loading - on/off
+   [ 05 ] >> Special Opportunities
+   [ 06 ] >> View My Activity
+   [ 07 ] >> Delete My Activity
+   [ 08 ] >> Restore AllHackingTools backup
+   [ 09 ] >> Create AllHackingTools backup
+   [ 10 ] >> Exit System - log out AllHackingTools
+   [ 11 ] >> Back To MainMenu - Back To Main Menu '| lolcat -p 1.8
