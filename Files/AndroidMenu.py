3cd1cfa198c7bc5d106a5c6a068911f3bffa25cd
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-06 11:08:26 +0300

    Create AndroidMenu.py

--- Files/AndroidMenu.py
+++ Files/AndroidMenu.py
@@ -9,10 +9,11 @@ os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mPyshell - Remote Access Trojan - RAT")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mWishfish - Powerful Tool For Grab Front Camera Snap Using A Link")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit System - Log out AllHackingTools")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mLockPhish - Tool for phishing attacks on the lock screen")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mExit System - Log out AllHackingTools")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
 
-op=int(raw_input("Ma1lHacK: "))
+op=int(raw_input("R6moteTRo1anR8t: "))
 
 if(op==1):
  os.system("clear")
@@ -21,10 +22,13 @@ elif(op==2):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd wishfish && ./wishfish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
- time.sleep(0.2)
- print("\033[1;31;40mQuiting System..")
- sys.exit()
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd lockphish && bash lockphish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==4):
+ time.sleep(0.2)
+ print("\033[1;31;40mQuiting System...")
+ sys.exit()
+elif(op==5):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
