6dd9ea5f4d1794256a44b096839c155395df1884
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-08-06 14:09:56 +0300

    Update CheckVersion.py

--- src/CheckVersion.py
+++ src/CheckVersion.py
@@ -33,7 +33,7 @@ import subprocess as subp
 row = []
 info = ''
 result = ''
-version = '2.3'
+version = '2.4'
 
 def ver_check():
 	print(G + '[+]' + C + ' Checking the AllHackingTools for updates....', end='')
