f4db278146f1182046ffc69be22f19d1f9eb06bf
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-08-25 22:34:22 +0300

    Create UserInfo.sh

---
+++ Tool/UserInfo.sh
@@ -0,0 +1,10 @@
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+DEFAULT_FG="$(printf '\033[39m')"  DEFAULT_BG="$(printf '\033[49m')"
+
+echo "${WHITE}[${RED}SYSTEM${WHITE}] ${ORANGE}Welcome user! ${GREEN}We are glad to see you. ${ORANGE}To run AllHackingTools, type the command: ${RED}msdc & msdconsole"
+echo "${WHITE}[${RED}SYSTEM${WHITE}] ${ORANGE}If the AllHackingTools is ${RED}not working ${ORANGE}properly, go to: ${GREEN}github.com/mishakorzik/AllHackingTools,${ORANGE} and see documentation."
+
+
