0f1816878440a60a558064ec297e81c718762218
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-01 18:13:59 +0300

    Create 2-5.php

---
+++ Tool/2-5.php
@@ -0,0 +1,76 @@
+<?php
+$white  = "\033[37m";
+$orange = "\033[33m";
+$green  = "\033[32m";
+$red    = "\033[31m";
+$cln    = "\e[0m";
+$bold   = "\e[1m";
+
+echo $white . "-";
+echo $orange . " New Features: \n";
+echo $white . "  \n";
+echo $white . "-";
+echo $orange . " 53a9007";
+echo $white . " [";
+echo $red . "bugs";
+echo $white . "]";
+echo $white . " A bug in the";
+echo $green . " Termux-Banner system has now been fixed";
+echo $white . "!";
+
+echo $white . "  \n";
+echo $white . "-";
+echo $orange . " 10a0509";
+echo $white . " [";
+echo $red . "bugs";
+echo $white . "]";
+echo $white . " Many faults have been fixed and now the";
+echo $green . " system works better";
+echo $white . "!";
+
+echo $white . "  \n";
+echo $white . "-";
+echo $orange . " 5we0011";
+echo $white . " [";
+echo $red . "debug";
+echo $white . "]";
+echo $white . " A new";
+echo $green . " debug mode has now been added";
+echo $white . " for testing and debugging";
+echo $white . "!";
+
+echo $white . "  \n";
+echo $white . "-";
+echo $orange . " 068q068";
+echo $white . " [";
+echo $red . "debug";
+echo $white . "]";
+echo $white . " Now when the system is malfunctioning it can be";
+echo $green . " fixed through debug mode";
+echo $white . "! \n";
+echo $white . "  \n";
+echo $white . "  \n";
+echo $white . "-";
+echo $orange . " New Scripts: \n";
+echo $white . "  \n";
+
+echo $white . "-";
+echo $orange . " 1t200e6";
+echo $white . " [";
+echo $red . "script";
+echo $white . "]";
+echo $white . " AllHackingTools still";
+echo $green . " uses php";
+echo $white . "!";
+
+echo $white . "  \n";
+echo $white . "-";
+echo $orange . " 4y00730";
+echo $white . " [";
+echo $red . "script";
+echo $white . "]";
+echo $white . " AllHackingTools is";
+echo $green . " still improved";
+echo $white . "!";
+
+?>
