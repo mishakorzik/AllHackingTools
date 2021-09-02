3e25ef06ecb2c8229dc2a2ff14b7574c7ebbbb4f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-02 17:47:31 +0300

    Update AllHackingTool.sh

--- src/AllHackingTool.sh
+++ src/AllHackingTool.sh
@@ -9,29 +9,6 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-echo '
-╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╱╱╱╱╱╭━━━━╮╱╱╭━━━╮╭╮╱╱╱╱╱╭╮
-┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱┃╭━━╯╱╭╯╰╮╱╱╱╱╱┃╭╮╭╮┃╱╱┃╭━╮┣╯╰╮╱╱╱╭╯╰╮
-┃╰━╯┣━┳━━┳━━┳━━╮┃╰━━┳━╋╮╭╋━━┳━╮╰╯┃┃┣┻━╮┃╰━━╋╮╭╋━━┳┻╮╭╯
-┃╭━━┫╭┫┃━┫━━┫━━┫┃╭━━┫╭╮┫┃┃┃━┫╭╯╱╱┃┃┃╭╮┃╰━━╮┃┃┃┃╭╮┃╭┫┃
-┃┃╱╱┃┃┃┃━╋━━┣━━┃┃╰━━┫┃┃┃╰┫┃━┫┃╱╱╱┃┃┃╰╯┃┃╰━╯┃┃╰┫╭╮┃┃┃╰╮
-╰╯╱╱╰╯╰━━┻━━┻━━╯╰━━━┻╯╰┻━┻━━┻╯╱╱╱╰╯╰━━╯╰━━━╯╰━┻╯╰┻╯╰━╯
-Please press enter to launch...'|lolcat -p 1.7 -a -d 7 -s 45.0
-read a1
-echo -e $b">"$w" verify modules: "$g"python2"$w
-apt-get install python
-echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
-echo -e $b">"$w" verify modules: "$g"pip2"$w
-apt-get install pip2
-echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
-echo -e $b">"$w" verify modules: "$g"requests"$w
-pip3 install requests
-echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
-echo -e $b">"$w" verify modules: "$g"php"$w
-apt-get install php
-echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
-echo -e $b">"$w" verify modules: "$g"wget"$w
-apt-get install wget
 cd
 cd
 cd AllHackingTools 
