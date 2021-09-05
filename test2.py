158228fcd840642cb64fc01af0c4783aeab83618
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-05 12:56:37 +0300

    Update and rename test.py to test2.py

---
+++ test2.py
@@ -0,0 +1,9 @@
+import random
+from time import sleep
+from rich.progress import track
+
+def do_step(step):
+    sleep(random.uniform(0.01, 0.1))
+
+for step in track(range(100), description="Загрузка пакетов ..."):
+    do_step(step)
