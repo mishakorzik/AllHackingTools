bae9eaacba00a186876ec3a871c4329da3370f9f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-05 14:04:40 +0300

    Update AnimationLoad1.sh

--- src/AnimationLoad1.sh
+++ src/AnimationLoad1.sh
@@ -6,110 +6,13 @@ w="\033[0m"
 o="\033[1;33m"
 printf "\e[1;92m"
 
-printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.05
-clear
-printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-clear
-printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-clear
-printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.02
-clear
-printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.02
-clear
-printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.03
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.05
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.005
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-help
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
-sleep 0.006
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
-sleep 0.006
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
-leep 0.1
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  Succesful!\n"
-cd
-cd 
+
 cd AllHackingTools 
+cd src
+python3 ProgressBar.py
 bash src/CheckWifi.sh
-sleep 2
+python3 Packages.py
+sleep 1
 clear
 sleep 0.1
 cd
