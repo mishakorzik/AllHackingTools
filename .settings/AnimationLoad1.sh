67ade341f6575745b05b4181691d7c982ff1e585
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-05 14:21:05 +0300

    Update AnimationLoad1.sh

--- .settings/AnimationLoad1.sh
+++ .settings/AnimationLoad1.sh
@@ -6,111 +6,17 @@ w="\033[0m"
 o="\033[1;33m"
 printf "\e[1;92m"
 
-printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
+cd
+cd
+cd AllHackingTools 
+cd src
+termux-vibrate -d 200
+python3 ProgressBar.py
+python3 Packages.py
+termux-vibrate -d 300
+sleep 2
 clear
-printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.05
-clear
-printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-clear
-printf "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-clear
-printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-termux-vibrate -d 200 -f
-clear
-printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.02
-clear
-printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.02
-clear
-printf "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ..\n"
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-termux-vibrate -d 100 -f
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
-sleep 0.03
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ..\n"
-sleep 0.05
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading .\n"
-sleep 0.005
-termux-vibrate -d 100 -f
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ..\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading .\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-help
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
-sleep 0.01
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading .\n"
-sleep 0.006
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ..\n"
-sleep 0.006
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
-leep 0.1
-clear
-printf "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  Succesfull!\n"
-sleep 0.2
 cd
 cd
 cd AllHackingTools
-termux-vibrate -d 100 -f
 python3 .check/Configuration.py
