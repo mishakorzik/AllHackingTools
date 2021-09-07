c4ea43c82bed4a56cc8c87af5f805613a3df3f8d
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-07 20:49:04 +0300

    Update AnimationLoad1.sh

--- src/AnimationLoad1.sh
+++ src/AnimationLoad1.sh
@@ -12,6 +12,25 @@ cd AllHackingTools
 cd src
 python3 ProgressBar.py
 python3 Packages.py
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+ping -c 1 google.com > /dev/null 2>&1
+if [[ "$?" == 0 ]]; then
+echo -e "$g[+]-[Internet Connection]———[ True ]"
+else
+echo -e "$r[-]-[Internet Connection]——[ False ]"
+echo -e "$rType: CTRL + C to exit"
+exit
+sleep 9999999
+sleep 9999999
+sleep 9999999
+exit
+fi
+
 sleep 2
 clear
 cd
