fa7c2b01bdd7071eb49ed94a0a0d42239ebbebde
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-10 22:56:53 +0300

    Delete ProgressBar1.py

--- src/ProgressBar1.py
+++
@@ -1,9 +0,0 @@
-import random
-from time import sleep
-from rich.progress import track
-
-def do_step(step):
-    sleep(random.uniform(000.001, 000.01))
-
-for step in track(range(100), description="Zsh loading"):
-    do_step(step)
