585ea704933ed9fa8beb62814c039a64ce14d47a
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-19 11:30:14 +0300

    Update IpMenu.py

--- Files/IpMenu.py
+++ Files/IpMenu.py
@@ -20,8 +20,9 @@ print("  \033[1;34m[ 10 ] >> \033[1;36;40mSeeker - Accurately Locate Smartphones
 print("  \033[1;34m[ 11 ] >> \033[1;36;40mReconDog - Reconnaissance Swiss Army Knife")
 print("  \033[1;34m[ 12 ] >> \033[1;36;40mD-Tech - Pentesting the Modern Web")
 print("  \033[1;34m[ 13 ] >> \033[1;36;40mWebKiller - Tool Information Gathering Write By Python")
-print("  \033[1;34m[ 14 ] >> \033[1;36;40mExit System - log out AllHackingTools")
-print("  \033[1;34m[ 15 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 14 ] >> \033[1;36;40mIpHack - Track Location With Live Address And City in Termux")
+print("  \033[1;34m[ 15 ] >> \033[1;36;40mExit System - log out AllHackingTools")
+print("  \033[1;34m[ 16 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("1nf0rmatI0n: "))
 
@@ -70,10 +71,13 @@ elif(op==13):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd webkiller && python3 webkiller.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==14):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Files && bash IpHack.sh && python2 MainMenu.py")
+elif(op==15):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==15):
+elif(op==16):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
