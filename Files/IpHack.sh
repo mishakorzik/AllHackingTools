7c8fe66320618621d06262c1d476e1ade39e1aed
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-19 11:28:47 +0300

    Create IpHack.sh

---
+++ Files/IpHack.sh
@@ -0,0 +1,20 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+clear
+cd
+cd AllHackingTools
+bash Logo.sh
+cd IpHack
+echo ""
+echo -e $b">>>"$w" Write the Ip Address: "$g"IpHack"$w
+read victimIP
+sleep 0.1
+echo -e $b">>>"$w" Please wait a moment: "$g"IpHack"$w
+sleep 0.6
+python IpHack.py -v $victimIP
+cd
+cd
+cd AllHackingTools
