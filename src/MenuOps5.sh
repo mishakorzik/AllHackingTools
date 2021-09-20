d93600ebf6fc04c556f4f994a51cb9357b249a43
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-20 18:25:40 +0300

    Update MenuOps5.sh

--- src/MenuOps5.sh
+++ src/MenuOps5.sh
@@ -4,12 +4,10 @@ echo '
    ├ Collects information from database
    ├ Find More Information
    ├ More Information In User
-   │
   [02] >> Exploitation Tools:
    ├ Selection of tools for operation and hacking
    ├ Tools for special operations 
    ├ blackmail utilities And human use
-   │
   [SKIP]┐
       ┌[03] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
       │[04] >> Web Attack Tools:  -  Tools for hacking websites and servers
@@ -39,12 +37,11 @@ echo '
    ├ Separate and other tools for hacking
   [17] >> Terminal Panel: 
    ├ Termux control and special features
-   │
   [SKIP]┐
       ┌[18] >> System Settings:  -  AllHackingTools control and special features
       │[19] >> System License:  -  View the license in AllHackingTools
   [-]─┘[20] >> Update System:  -  Update system and all tools & menu 
        [21] >> About System:  -  View system information and developer
        [22] >> Exit System:  -  Log out of the AllHackingTools system 
-       [23] >> Log Out Terminal  -  Log out in AllHackingTools And Terminal'|lolcat -p 2.0
+       [23] >> Log Out Terminal  -  Log out in AllHackingTools And Terminal'|lolcat -p 4.6
 
