b93374495a64db1dce01a57a5e98f9cd222ab11b
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-21 15:17:13 +0300

    Update starterUp.sh

--- src/starterUp.sh
+++ src/starterUp.sh
@@ -1,10 +1,17 @@
-python3 ProgressBar.py
-python3 Packages.py
 red='\e[1;31m'
 default='\e[0m'
 yellow='\e[0;33m'
 orange='\e[38;5;166m'
 green='\033[92m'
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+printf "\e[1;92m"
+
+python3 ProgressBar.py
+python3 Packages.py
 
 ping -c 1 google.com > /dev/null 2>&1
 if [[ "$?" == 0 ]]; then
