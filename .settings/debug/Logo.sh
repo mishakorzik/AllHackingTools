1f8e445897381eb4098af16015a05969075e809c
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-22 21:35:28 +0300

    Update Logo.sh

--- .settings/debug/Logo.sh
+++ .settings/debug/Logo.sh
@@ -12,4 +12,4 @@ echo '
 [+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
 [+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
 [+]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[DEBUG]
-'|lolcat -p 1.0 -a -d 2 -s 20.0
+'|lolcat -p 3.0 -a -d 2 -s 20.0
