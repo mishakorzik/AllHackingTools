ef1b8e9353b356eea4c1db23a7010b640f44a241
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-24 14:23:18 +0300

    Update menu.py

--- Files/Proxy/menu.py
+++ Files/Proxy/menu.py
@@ -160,13 +160,13 @@ if(op==1):
     except:
         pass
 elif(op==2):
- generate("https://us-proxy.org",65) 
+ generate("https://us-proxy.org",25) 
 elif(op==3):
- generate("https://free-proxy-list.net/uk-proxy.html",50)
+ generate("https://free-proxy-list.net/uk-proxy.html",20)
 elif(op==4):
- generate("https://www.sslproxies.org",50)
+ generate("https://www.sslproxies.org",15)
 elif(op==5):
- generate("https://free-proxy-list.net/anonymous-proxy.html",50)
+ generate("https://free-proxy-list.net/anonymous-proxy.html",15)
 elif(op==6):
  socks(85)
  print " "+Blue+"177.206.186.9:8080   SOCKS4   Brazil   Elite"
