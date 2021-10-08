dca36fe584d3c88327e17262a025da1a18d5a93a
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-08 19:25:08 +0300

    Update CheckVersion.py

--- src/CheckVersion.py
+++ src/CheckVersion.py
@@ -33,7 +33,7 @@ import subprocess as subp
 row = []
 info = ''
 result = ''
-version = '2.5.7'
+version = '2.6.3'
 
 def ver_check():
 	print(G + '[+]' + C + ' Checking the AllHackingTools for updates....', end='')
