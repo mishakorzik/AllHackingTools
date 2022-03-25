a2817ef35fa864fb0b32d65cd40cd8b8305ec151
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-03-25 20:49:05 +0200

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -107,6 +107,14 @@ chmod +x msdServer
 chmod +x folder
 cd
 cd
+rm -rf ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+rm ngrok-stable-linux-amd64.zip
+chmod 755 ngrok
+mv ngrok /usr/bin/
+cd
+cd
 echo '
 ██████╗░░█████╗░███╗░░██╗███████╗
 ██╔══██╗██╔══██╗████╗░██║██╔════╝
