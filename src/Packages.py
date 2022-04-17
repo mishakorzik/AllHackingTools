9c58e723b4aa04159ba48927746eec5ceb8abd2f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-04-17 11:56:51 +0300

    Update Packages.py

--- src/Packages.py
+++ src/Packages.py
@@ -7,15 +7,5 @@ tasks = [f"data {n}" for n in range(1, 4)]
 with console.status("[bold green]I perform the task ...[/bold green]") as status:
     while tasks:
         task = tasks.pop(0)
-        sleep(1)
-        console.log(f"{task} done!")
-
-console1 = Console()
-tasks1 = [f"data {n}" for n in range(4, 8)]
-
-with console1.status("[bold green]I perform the task ...[/bold green]") as status:
-    while tasks1:
-        task = tasks1.pop(0)
         sleep(0.7)
-        console1.log(f"{task} done!")
-
+        console.log(f"{task} done!")
