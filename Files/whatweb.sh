f9a08a3abf925546d605a4e55ed476eb4d40a6c0
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-08 12:21:51 +0300

    Create whatweb.sh

---
+++ Files/whatweb.sh
@@ -0,0 +1,19 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+clear
+cd
+cd AllHackingTools
+cd WhatWeb
+clear
+./whatweb
+echo "Please enter all your required settings to run the program, for example: reddit.com"
+echo -e $b">>>"$w" Consoler "$g"WhatWeb"$w
+read opt
+sleep 0.1
+./whatweb $opt
+cd
+cd
+cd AllHackingTools
