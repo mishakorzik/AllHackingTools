c2481b3825c6d790fc690e22ebe9b65d41766dac
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-08 12:19:22 +0300

    Update WebMenu.py

--- Files/WebMenu.py
+++ Files/WebMenu.py
@@ -15,8 +15,9 @@ print("  \033[1;34m[ 05 ] >> \033[1;36;40mWebSploit - An advanced MiTM Framework
 print("  \033[1;34m[ 06 ] >> \033[1;36;40mSqlmate - A friend of SQLmap which will do what you always expected from SQLmap")
 print("  \033[1;34m[ 07 ] >> \033[1;36;40mSH33LL - A Easy Shell scanner")
 print("  \033[1;34m[ 09 ] >> \033[1;36;40mUltraDDos - Tool for ddos web sites")
-print("  \033[1;34m[ 10 ] >> \033[1;36;40mExit System - log out AllHackingTools")
-print("  \033[1;34m[ 11 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 10 ] >> \033[1;36;40mWhatWeb - Next generation web scanner")
+print("  \033[1;34m[ 11 ] >> \033[1;36;40mExit System - log out AllHackingTools")
+print("  \033[1;34m[ 12 ] >> \033[1;36;40mBack To MainMenu")
 
 
 op=int(raw_input("Web-Hack1Ng: "))
@@ -49,10 +50,13 @@ elif(op==9):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd Ultra-DDos && python2 main.py && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==10):
+ os.system("clear")
+ os.system("cd && cd AllHackingTools && cd Files && bash whatweb.sh && echo done! && sleep 4 && cd && cd AllHackingTools && python2 MainMenu.py")
+elif(op==11):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==11):
+elif(op==12):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
