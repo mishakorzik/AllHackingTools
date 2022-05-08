e97a94ea594dd77c33cb12cc5cc70d18247c8a36
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-08 11:39:12 +0300

    Create Nikto.sh

---
+++ Files/Nikto.sh
@@ -0,0 +1,19 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+clear
+cd
+cd AllHackingTools
+cd nikto/program
+clear
+./nikto.pl
+echo "Please enter all your required settings to run the program, for example: -h https://www.example.com"
+echo -e $b">>>"$w"Console/> "$g"Nikto"$w
+read opt
+sleep 0.1
+./nikto.pl opt
+cd
+cd
+cd AllHackingTools
