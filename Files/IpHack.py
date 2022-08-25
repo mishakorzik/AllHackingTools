4c5cb4c0383d539292f257defeca56007d5d9470
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-08-25 14:49:58 +0300

    Create IpHack.py

---
+++ Files/IpHack.py
@@ -0,0 +1,17 @@
+from iphack import *
+
+print("01: Ip Address")
+print("02: Proxy Checker")
+print("03: Proxy Generator")
+
+op = input("Select Option: ")
+
+if op == "1" or op == "01":
+    ip = input("While ip address: ")
+    ip.address(ip)
+if op == "2" or op == "02":
+    ip_p = input("While proxy ip: ")
+    port_p = input("While proxy ip: ")
+    check.proxy(ip_p, port_p)
+if op == "3" or op == "03":
+    ip.proxy(False)
