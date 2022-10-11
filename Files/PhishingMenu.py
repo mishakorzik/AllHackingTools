2139d2fc846a03a0b1f6f35c0e469f65fc21b09a
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-10-11 11:27:18 +0300

    Update PhishingMenu.py

--- Files/PhishingMenu.py
+++ Files/PhishingMenu.py
@@ -17,8 +17,9 @@ print("  \033[1;34m[ 07 ] >> \033[1;36;40mSeeker - Accurately Locate Smartphones
 print("  \033[1;34m[ 08 ] >> \033[1;36;40mAIOPhish - A Social Toolkit for phishing")
 print("  \033[1;34m[ 09 ] >> \033[1;36;40mISeeYou - Find the exact location of the users")
 print("  \033[1;34m[ 10 ] >> \033[1;36;40mBlackEye - The ultimate phishing tool with 38 websites available!")
-print("  \033[1;34m[ 11 ] >> \033[1;36;40mExit System - log out AllHackingTools")
-print("  \033[1;34m[ 12 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 11 ] >> \033[1;36;40mNPhish - Phishing Tool with 60+ Phishing Sites")
+print("  \033[1;34m[ 12 ] >> \033[1;36;40mExit System - log out AllHackingTools")
+print("  \033[1;34m[ 13 ] >> \033[1;36;40mBack To MainMenu")
 
 op=int(raw_input("Phish1ng: "))
 
@@ -58,10 +59,13 @@ elif(op==10):
  os.system("clear")
  os.system("cd && cd AllHackingTools && cd blackeye && bash blackeye.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==11):
+ os.system("clear")
+ os.system("NPhish")
+elif(op==12):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
-elif(op==12):
+elif(op==13):
  os.system("cd")
  os.system("cd AllHackingTools")
  os.system("python2 MainMenu.py")
