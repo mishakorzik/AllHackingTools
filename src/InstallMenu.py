633accbe757c2f0e692cac195119a467b79caf35
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2023-01-14 11:01:30 +0200

    Update InstallMenu.py

--- src/InstallMenu.py
+++ src/InstallMenu.py
@@ -9,9 +9,8 @@ os.system("cd && cd AllHackingTools && bash src/LiteLogo.sh")
 
 print("")
 print("  \033[1;34m[01] \033[1;36;40mDefault  -  Standart installing and default")
-print("  \033[1;34m[02] \033[1;36;40mRestore  -  Restore AllHackingTools in backup")
-print("  \033[1;34m[03] \033[1;36;40mCoded  -  Install via encoded code NOT RECOMMENDED")
-print("  \033[1;34m[04] \033[1;36;40mQuit  -  Exit and do not download AllHackingTools")
+print("  \033[1;34m[02] \033[1;36;40mCoded  -  Install via encoded code NOT RECOMMENDED")
+print("  \033[1;34m[02] \033[1;36;40mQuit  -  Exit and do not download AllHackingTools")
 
 op=int(raw_input("1nStall: "))
 
@@ -20,14 +19,8 @@ if(op==1):
  os.system("cd && cd AllHackingTools && bash Files/Modules.sh")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && bash src/LiteLogo.sh")
- os.system("cd && rm -rf AutoUpdateMyTools && cd $HOME && git clone https://github.com/mishakorzik/AutoUpdateMyTools && cd")
- os.system("cd AutoUpdateMyTools && cd Files")
- os.system("bash RestoreAllHackingToolBackup.sh")
-elif(op==3):
- os.system("clear")
  os.system("cd && cd AllHackingTools && bash Files/CodedModules.sh")
-elif(op==4):
+elif(op==2):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting AllHackingTool Installer...")
  sys.exit()
