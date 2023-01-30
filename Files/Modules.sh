0c7aa2e822fde9362d4ab477e03b281d1b75f3ad
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2023-01-30 10:58:14 +0200

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -109,6 +109,8 @@ cd
 pip install NPhish
 cd
 pip install heconsole
+pip install future
+pip install rich
 rm -rf ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
