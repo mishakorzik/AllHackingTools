2f085b983170d1643935b0534c74714afbcbab76
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2023-06-27 09:26:58 +0300

    Update UserInfo.py

--- Tool/UserInfo.py
+++ Tool/UserInfo.py
@@ -1,4 +1,4 @@
 
-print(f"[\x1b[38;5;9mSYSTEM\x1b[38;5;255m] \x1b[38;5;227mWelcome user! .\x1b[38;5;119mWe are glad to see you. \x1b[38;5;227mTo run AllHackingTools, type the command: \x1b[38;5;9mmsdc")
-print(f"[\x1b[38;5;9mSYSTEM\x1b[38;5;255m] \x1b[38;5;227mIf AllHackingTools does \x1b[38;5;9mnot work,\x1b[38;5;227m go to: .\x1b[38;5;119mgithub.com/mishakorzik/AllHackingTools")
+print(f"[\x1b[38;5;9mSYSTEM\x1b[38;5;255m] \x1b[38;5;227mWelcome user! \x1b[38;5;119mWe are glad to see you. \x1b[38;5;227mTo run AllHackingTools, type the command: \x1b[38;5;9mmsdc")
+print(f"[\x1b[38;5;9mSYSTEM\x1b[38;5;255m] \x1b[38;5;227mIf AllHackingTools does \x1b[38;5;9mnot work,\x1b[38;5;227m go to: \x1b[38;5;119mgithub.com/mishakorzik/AllHackingTools")
 print(f"[\x1b[38;5;9mSYSTEM\x1b[38;5;255m] \x1b[38;5;227mAllHackingTools donate: \x1b[38;5;119mhttps://www.buymeacoffee.com/misakorzik")
