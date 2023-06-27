d607f598b692ed41a837202eb2161fd2c12a14f9
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2023-06-27 09:20:01 +0300

    Update UserInfo.py

--- Tool/UserInfo.py
+++ Tool/UserInfo.py
@@ -1,4 +1,4 @@
-from heconsole import console
-console.arrowLog(0, "[.REDSYSTEM.WHITE] .YELLOWWelcome user! .GREENWe are glad to see you. .YELLOWTo run AllHackingTools, type the command: .REDmsdc", showTime=False)
-console.arrowLog(0, "[.REDSYSTEM.WHITE] .YELLOWIf AllHackingTools does .REDnot work,.YELLOW go to: .GREENgithub.com/mishakorzik/AllHackingTools", showTime=False)
-console.arrowLog(0, "[.REDSYSTEM.WHITE] .YELLOWAllHackingTools donate: .GREENhttps://www.buymeacoffee.com/misakorzik", showTime=False)
+
+print("[.REDSYSTEM.WHITE] .YELLOWWelcome user! .GREENWe are glad to see you. .YELLOWTo run AllHackingTools, type the command: .REDmsdc", showTime=False)
+print("[.REDSYSTEM.WHITE] .YELLOWIf AllHackingTools does .REDnot work,.YELLOW go to: .GREENgithub.com/mishakorzik/AllHackingTools", showTime=False)
+print("[.REDSYSTEM.WHITE] .YELLOWAllHackingTools donate: .GREENhttps://www.buymeacoffee.com/misakorzik", showTime=False)
