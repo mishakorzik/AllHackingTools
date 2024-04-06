5b52216e7fdb65bb95c300dedb0b8dd72bf77cff
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2024-04-06 15:16:40 +0300

    Update SocialFiles.sh

--- Files/SocialFiles.sh
+++ Files/SocialFiles.sh
@@ -1,11 +1,4 @@
 cd
-cd 
-cd AllHackingTools
-git clone https://github.com/evildevill/instahack
-cd 
-cd instahack
-bash setup
-cd
 cd
 cd AllHackingTools
 git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
