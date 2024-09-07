a44d5dd1b3241f713c42999aed0b96f598bb309f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2024-09-08 00:39:55 +0300

    Update IpHack.py

--- Files/IpHack.py
+++ Files/IpHack.py
@@ -1,4 +1,5 @@
 from iphack import *
+import time
 
 print("01: Ip Address")
 print("02: Proxy Checker")
@@ -9,9 +10,12 @@ op = input("Select Option: ")
 if op == "1" or op == "01":
     ip_adr = input("While IP: ")
     ip.address(ip_adr)
+    time.sleep(60)
 if op == "2" or op == "02":
     ip_p = input("While proxy ip: ")
     port_p = input("While proxy port: ")
     check.proxy(ip_p, port_p)
+    time.sleep(60)
 if op == "3" or op == "03":
     ip.proxy(False)
+    time.sleep(60)
